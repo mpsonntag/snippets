1e67457396dfdc63868535fa435a18cafb2b717d
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 15:03:55 +0200

    [odml/example] fix create 1.3 bugs

--- python/odml/example_create_1.3.py
+++ python/odml/example_create_1.3.py
@@ -1,6 +1,12 @@
 import odml
 import os
 
+from distutils.version import LooseVersion as CheckVer
+
+if not CheckVer(odml.__version__) < CheckVer('1.4.0'):
+    print("odml version 1.3 or lesser required")
+    quit()
+
 wdir = "/home/msonntag/Chaos/work/x_odml/tmp"
 fn_base = "1.3.3_example.%s"
 
@@ -28,22 +34,28 @@ prop = odml.Property(name="prop one", value="", definition="def prop1",
                      dependency="dep p1", dependency_value="dep val p1")
 
 prop = odml.Property(name="prop one", value="", definition="def prop1",
-                     dependency="dep p1", dependency_value="dep val p1", parent=doc.sections[0])
+                     dependency="dep p1", dependency_value="dep val p1")
+doc.sections[0].append(prop)
 
 prop = odml.Property(name="prop two", value="", definition="def prop2",
-                     dependency="dep p2", dependency_value="dep val p2", parent=doc.sections[0])
+                     dependency="dep p2", dependency_value="dep val p2")
+doc.sections[0].append(prop)
 
 prop = odml.Property(name="prop three", value="", definition="def prop3",
-                     dependency="dep p3", dependency_value="dep val p3", parent=doc.sections[0])
+                     dependency="dep p3", dependency_value="dep val p3")
+doc.sections[0].append(prop)
 
 prop = odml.Property(name="prop one", value="", definition="def prop1",
-                     dependency="dep p1", dependency_value="dep val p1", parent=doc.sections[0].sections[0])
+                     dependency="dep p1", dependency_value="dep val p1")
+doc.sections[0].sections[0].append(prop)
 
 prop = odml.Property(name="prop three", value="", definition="def prop3",
-                     dependency="dep p3", dependency_value="dep val p3", parent=doc.sections[0].sections[0])
+                     dependency="dep p3", dependency_value="dep val p3")
+doc.sections[0].sections[0].append(prop)
 
 prop = odml.Property(name="prop two", value="", definition="def prop2",
-                     dependency="dep p2", dependency_value="dep val p2", parent=doc.sections[0].sections[0])
+                     dependency="dep p2", dependency_value="dep val p2")
+doc.sections[0].sections[0].append(prop)
 
 val = odml.Value(value=["1", "2", "3", "4", "5"], uncertainty=12, unit="arbitrary", dtype=odml.DType.string,
                  definition="def val 1", reference="ref val 1", filename="filename val 1",
