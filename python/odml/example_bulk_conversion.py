7e4cbfb56fa9b0c23b65aab9f818b894ff43085a
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 14:46:34 +0200

    [odml/example] Refactor bulk conversion example

--- python/odml/example_bulk_conversion.py
+++ python/odml/example_bulk_conversion.py
@@ -1,11 +1,11 @@
+import os
 import odml.tools as oto
 
-main = "/home/msonntag/Chaos/DL/conv/"
-folder_v1_0 = main
-folder_v1_1 = main + "out/"
+wdir = "/home/msonntag/Chaos/work/x_odml/file_dump/conv"
+folder_v1_1 = os.path.join(wdir, "out")
 
 # Conversion of v1.0 to v1.1 files
 # Will overwrite existing files
 fc = oto.format_converter.FormatConverter()
-fc.convert_dir(folder_v1_0, folder_v1_1, False, "v1_1")
+fc.convert_dir(wdir, folder_v1_1, False, "v1_1")
 
