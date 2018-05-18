110e6cc0c0f7a8d3fc01943577416cb464efbdbc
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 18:10:00 +0200

    [odml/test install] Refactor directory structure

--- python/odml/install_basic_tests.py
+++ python/odml/install_basic_tests.py
@@ -5,33 +5,38 @@ from odml.tools.version_converter import VersionConverter
 
 workdir = "/home/msonntag/Chaos/work/x_odml"
 slf = "save_load"
-complex = "complex_files"
+new_fn_base = "1.4_testfile.%s"
+
+convdir = "convert"
 conv_fn_base = "1.3_convert.%s"
-conv_folder = "converted"
-new_files = "1.4_testfile.%s"
+
+compdir = "complex"
+comp_fn_base = "1.4_example.%s"
 
 print("Version '%s'\nInstalled at '%s'\n" % (odml.__version__, odml.__file__))
 
-print("Create document\n")
+print("Working in directory '%s'\n" % workdir)
+
+print("\nCreate document\n")
 doc = odml.Document(author="HPL")
 sec = odml.Section(name="sec", parent=doc)
 prop = odml.Property(name="prop", parent=sec)
 subsec1 = odml.Section(name="subsec", parent=sec)
 subsec2 = odml.Section(parent=sec)
 
-print("Saving document\n%s\n" % doc)
-odml.save(doc, os.path.join(workdir, slf, (new_files % 'x')))
-odml.save(doc, os.path.join(workdir, slf, (new_files % 'j')), 'JSON')
-odml.save(doc, os.path.join(workdir, slf, (new_files % 'r')), 'RDF')
-odml.save(doc, os.path.join(workdir, slf, (new_files % 'y')), 'YAML')
+print("\nSaving document\n%s\n" % doc)
+odml.save(doc, os.path.join(workdir, slf, (new_fn_base % 'x')))
+odml.save(doc, os.path.join(workdir, slf, (new_fn_base % 'j')), 'JSON')
+odml.save(doc, os.path.join(workdir, slf, (new_fn_base % 'r')), 'RDF')
+odml.save(doc, os.path.join(workdir, slf, (new_fn_base % 'y')), 'YAML')
 
-print("Loading document\n")
-xdoc = odml.load(os.path.join(workdir, slf, (new_files % 'x')))
-jdoc = odml.load(os.path.join(workdir, slf, (new_files % 'j')), 'JSON')
-ydoc = odml.load(os.path.join(workdir, slf, (new_files % 'y')), 'YAML')
-rdoc = odml.tools.odmlparser.ODMLReader('RDF').from_file(os.path.join(workdir, slf, (new_files % 'r')), 'turtle')[0]
+print("\nLoading document\n")
+xdoc = odml.load(os.path.join(workdir, slf, (new_fn_base % 'x')))
+jdoc = odml.load(os.path.join(workdir, slf, (new_fn_base % 'j')), 'JSON')
+ydoc = odml.load(os.path.join(workdir, slf, (new_fn_base % 'y')), 'YAML')
+rdoc = odml.tools.odmlparser.ODMLReader('RDF').from_file(os.path.join(workdir, slf, (new_fn_base % 'r')), 'turtle')[0]
 
-print("Testing equality\n")
+print("\nTesting equality\n")
 assert(doc == xdoc)
 assert(doc == jdoc)
 #assert(doc == rdoc)
@@ -55,17 +60,19 @@ assert(prop == ydoc[0].properties[0])
 print("\nTest format conversion from XML\n")
 conv_file = conv_fn_base % "xml"
 _ = VersionConverter(
-    os.path.join(workdir, conv_file)).write_to_file(
-    os.path.join(workdir, conv_folder, "1.4_%s" % conv_file))
+    os.path.join(workdir, convdir, conv_file)).write_to_file(
+    os.path.join(workdir, convdir, "1.4_%s" % conv_file))
 
 print("\nTest format conversion from JSON\n")
 conv_file = conv_fn_base % "json"
 _ = VersionConverter(
-    os.path.join(workdir, conv_file)).write_to_file(
-    os.path.join(workdir, conv_folder, "1.4_%s" % conv_file), 'JSON')
+    os.path.join(workdir, convdir, conv_file)).write_to_file(
+    os.path.join(workdir, convdir, "1.4_%s" % conv_file), 'JSON')
 
 print("\nTest format conversion from YAML\n")
 conv_file = conv_fn_base % "yaml"
 _ = VersionConverter(
-    os.path.join(workdir, conv_file)).write_to_file(
-    os.path.join(workdir, conv_folder, "1.4_%s" % conv_file), 'YAML')
+    os.path.join(workdir, convdir, conv_file)).write_to_file(
+    os.path.join(workdir, convdir, "1.4_%s" % conv_file), 'YAML')
+
+print("\nDone...\n")
