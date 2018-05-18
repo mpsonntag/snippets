2c58db550e5a52d73bc585172ad8ab0b11486566
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 14:23:07 +0200

    [odml/testinstall] refactor file structure

--- python/odml/odml_install_basic_tests.py
+++ python/odml/odml_install_basic_tests.py
@@ -2,7 +2,10 @@ import os
 import odml
 
 workdir = "/home/msonntag/Chaos/work/x_testodml"
-fn = "1.4_testfile.%s"
+slf = "save_load"
+complex = "complex_files"
+conv_folder = "converted"
+new_files = "1.4_testfile.%s"
 
 print("Version '%s'\nInstalled at '%s'\n" % (odml.__version__, odml.__file__))
 
@@ -14,16 +17,16 @@ subsec1 = odml.Section(name="subsec", parent=sec)
 subsec2 = odml.Section(parent=sec)
 
 print("Saving document\n%s\n" % doc)
-odml.save(doc, os.path.join(workdir, (fn % 'x')))
-odml.save(doc, os.path.join(workdir, (fn % 'j')), 'JSON')
-odml.save(doc, os.path.join(workdir, (fn % 'r')), 'RDF')
-odml.save(doc, os.path.join(workdir, (fn % 'y')), 'YAML')
+odml.save(doc, os.path.join(workdir, slf, (new_files % 'x')))
+odml.save(doc, os.path.join(workdir, slf, (new_files % 'j')), 'JSON')
+odml.save(doc, os.path.join(workdir, slf, (new_files % 'r')), 'RDF')
+odml.save(doc, os.path.join(workdir, slf, (new_files % 'y')), 'YAML')
 
 print("Loading document\n")
-xdoc = odml.load(os.path.join(workdir, (fn % 'x')))
-jdoc = odml.load(os.path.join(workdir, (fn % 'j')), 'JSON')
-ydoc = odml.load(os.path.join(workdir, (fn % 'y')), 'YAML')
-rdoc = odml.tools.odmlparser.ODMLReader('RDF').from_file(os.path.join(workdir, (fn % 'r')), 'turtle')[0]
+xdoc = odml.load(os.path.join(workdir, slf, (new_files % 'x')))
+jdoc = odml.load(os.path.join(workdir, slf, (new_files % 'j')), 'JSON')
+ydoc = odml.load(os.path.join(workdir, slf, (new_files % 'y')), 'YAML')
+rdoc = odml.tools.odmlparser.ODMLReader('RDF').from_file(os.path.join(workdir, slf, (new_files % 'r')), 'turtle')[0]
 
 print("Testing equality\n")
 assert(doc == xdoc)
@@ -45,8 +48,3 @@ assert(prop == xdoc[0].properties[0])
 assert(prop == jdoc[0].properties[0])
 assert(prop == rdoc[0].properties[0])
 assert(prop == ydoc[0].properties[0])
-
-print("Test loading complex files")
-xdoc = odml.load(os.path.join(workdir, ''))
-
-
