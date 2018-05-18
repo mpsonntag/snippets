24100184c3a6f8dced3218d3db2efb6cee3bc16d
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 14:43:43 +0200

    [odml/example] Refactor rdf converter example

--- python/odml/example_rdf_conversion.py
+++ python/odml/example_rdf_conversion.py
@@ -1,15 +1,14 @@
+import os
 import odml.tools as oto
 
-main = "/home/msonntag/Chaos/DL/conv/"
-folder_v1_0 = main
-folder_v1_1 = main + "out/"
-folder_rdf = main + "rdf/"
+wdir = "/home/msonntag/Chaos/work/x_odml/file_dump/conv"
+folder_v1_1 = os.path.join(wdir, "out")
+folder_rdf = os.path.join(wdir, "rdf")
 
-fname_short = folder_v1_1 + "spikeLFP_metadata080707_short_v1.0.xml"
-frdf_short = folder_rdf +"spikeLFP_metadata080707_short.ttl"
+fname_short = os.path.join(folder_v1_1, "spikeLFP_metadata080707_short_v1.0.xml")
+frdf_short = os.path.join(folder_rdf, "spikeLFP_metadata080707_short.ttl")
 
 # requires odML v1.1 files
-doc = oto.odmlparser.ODMLReader(parser='XML').fromFile(open(fname_short))
+doc = oto.odmlparser.ODMLReader(parser='XML').from_file(fname_short)
 f = oto.rdf_converter.RDFWriter(doc)
 f.write_file(frdf_short, "turtle")
-
