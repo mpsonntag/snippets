f771ac2ee5b48e6b7a9f4c7b2cb21dba734d0888
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 16:08:10 +0200

    [python/envs] Add pip uninstall to odml cleanup

--- python/envs/pyenv_cleanup_odml.sh
+++ python/envs/pyenv_cleanup_odml.sh
@@ -2,6 +2,24 @@
 
 PYENV='/home/msonntag/Chaos/software/pyvirtualenv/'
 
+echo "Activating pyenv"
+source $PYENV"pymain/bin/activate"
+
+echo "Trying to pip uninstall odml"
+pip uninstall odml-ui
+pip uninstall odml
+
+deactivate
+
+echo "Activating pyenv3"
+source $PYENV"py3main/bin/activate"
+
+echo "Trying to pip uninstall odml"
+pip uninstall odml-ui
+pip uninstall odml
+
+deactivate
+
 echo "Removing odML dist folder"
 find $PYENV -name "odML*" -exec rm -r -v {} \;
 
