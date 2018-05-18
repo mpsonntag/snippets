26facc3740d6e7f91dc2d472a6b1dff9b83f6208
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 14:37:48 +0200

    [odml/example] Refactor version converter example

--- python/odml/example_version_converter.py
+++ python/odml/example_version_converter.py
@@ -4,15 +4,16 @@ import odml.tools as oto
 from odml.tools.version_converter import VersionConverter
 
 wdir = "/home/msonntag/Chaos/work/x_odml/file_dump"
-fin = os.path.join(wdir, ()) "" main +"tmp.xml"
-fout = main+"tmpout.xml"
+fn = "1.3.3_convert%s.xml"
+fin = fn % ""
+fout = fn % "_converted"
 
 # Conversion of odML v1 to v1.1
-vc = VersionConverter(fin)
+vc = VersionConverter(os.path.join(wdir, fin))
 
 # either write to file
 print("Converting '%s' ..." % vc.filename)
-vc.write_to_file(fout)
+vc.write_to_file(os.path.join(wdir, fout))
 
 # or pass it to the python xml parser
 doc = oto.odmlparser.ODMLReader(parser='XML').from_string(str(vc))
