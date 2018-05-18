ddc5922284d95851bc8df001b7500bf9ad28693c
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 18:52:59 +0200

    [python/envs] Update cleanup conda path handling

--- python/envs/conda_reset_odml.sh
+++ python/envs/conda_reset_odml.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
+CONDABIN='/home/msonntag/Chaos/software/mconda2/bin'
+
 echo -- Running odml_conda_deps_reset.sh
+echo -- make sure we are clean and not in an environment.
+source deactivate
 
 echo -- Cleanup previous environments
 
-/home/msonntag/Chaos/software/mconda2/bin/conda remove -n o2 --all -y
-/home/msonntag/Chaos/software/mconda2/bin/conda remove -n oui2 --all -y
-/home/msonntag/Chaos/software/mconda2/bin/conda remove -n o3 --all -y
-/home/msonntag/Chaos/software/mconda2/bin/conda remove -n oui3 --all -y
+$CONDABIN/conda remove -n o2 --all -y
+$CONDABIN/conda remove -n ot2 --all -y
+$CONDABIN/conda remove -n o3 --all -y
+$CONDABIN/conda remove -n ot3 --all -y
 
 echo -- Create test environments
 
-/home/msonntag/Chaos/software/mconda2/bin/conda create -n o2 python=2.7 -y
-/home/msonntag/Chaos/software/mconda2/bin/conda create -n oui2 python=2.7 -y
-/home/msonntag/Chaos/software/mconda2/bin/conda create -n o3 python=3.5 -y
-/home/msonntag/Chaos/software/mconda2/bin/conda create -n oui3 python=3.5 -y
+$CONDABIN/conda create -n o2 python=2.7 -y
+$CONDABIN/conda create -n ot2 python=2.7 -y
+$CONDABIN/conda create -n o3 python=3.5 -y
+$CONDABIN/conda create -n ot3 python=3.5 -y
 
 echo -- Install dependencies
 
-source /home/msonntag/Chaos/software/mconda2/bin/activate oui2
+source $CONDABIN/activate ot2
 
 conda install -c pkgw/label/superseded gtk3 -y
 conda install -c conda-forge pygobject -y
 conda install -c conda-forge gdk-pixbuf -y
 conda install -c pkgw-forge adwaita-icon-theme -y
 
-source /home/msonntag/Chaos/software/mconda2/bin/activate oui3
+source $CONDABIN/activate ot3
 
 conda install -c pkgw/label/superseded gtk3 -y
 conda install -c conda-forge pygobject -y
@@ -34,5 +38,5 @@ conda install -c pkgw-forge adwaita-icon-theme -y
 
 echo -- List current environments
 
-/home/msonntag/Chaos/software/mconda2/bin/conda info --envs
+$CONDABIN/conda info --envs
 
