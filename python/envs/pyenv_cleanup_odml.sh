ddcb9886cec52d12c8a17be97a72bed19cad7ded
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-18 16:07:07 +0200

    [python/envs] Add remove custom odml install

--- python/envs/pyenv_cleanup_odml.sh
+++ python/envs/pyenv_cleanup_odml.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 PYENV='/home/msonntag/Chaos/software/pyvirtualenv/'
+
+echo "Removing odML dist folder"
+find $PYENV -name "odML*" -exec rm -r -v {} \;
+
+echo "Removing odml folder and additional files"
+find $PYENV -name "odml*" -exec rm -r -v {} \;
