a631e17442468899a4c292fb5919876120f0ea4c
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-22 20:11:48 +0200

    [python/envs] Simplify conda cleanup

--- python/envs/conda_cleanup_odml.sh
+++ python/envs/conda_cleanup_odml.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-CONDABIN=$HOME'/Chaos/software/mconda2/bin'
+CONDA=$HOME'/Chaos/software/mconda2/bin/conda'
 if [ $(which conda) ]; then
-    CONDABIN=$(which conda)"/bin"
+    CONDA=$(which conda)
 fi
 
 echo "-- Running conda_cleanup_odml.sh"
-echo "-- Using conda at $CONDABIN"
+echo "-- Using conda at $CONDA"
 
 echo "-- make sure we are clean and not in an environment."
 source deactivate
 
 echo "-- Cleanup previous environments"
 
-$CONDABIN/conda remove -n o2 --all -y
-$CONDABIN/conda remove -n ot2 --all -y
-$CONDABIN/conda remove -n o3 --all -y
-$CONDABIN/conda remove -n ot3 --all -y
+$CONDA remove -n o2 --all -y
+$CONDA remove -n ot2 --all -y
+$CONDA remove -n o3 --all -y
+$CONDA remove -n ot3 --all -y
 
 echo "-- Create empty test environments"
 
-$CONDABIN/conda create -n o2 python=2.7 -y
-$CONDABIN/conda create -n ot2 python=2.7 -y
-$CONDABIN/conda create -n o3 python=3.5 -y
-$CONDABIN/conda create -n ot3 python=3.5 -y
+$CONDA create -n o2 python=2.7 -y
+$CONDA create -n ot2 python=2.7 -y
+$CONDA create -n o3 python=3.5 -y
+$CONDA create -n ot3 python=3.5 -y
 
 echo "-- List current environments"
 
-$CONDABIN/conda info --envs
+$CONDA info --envs
 
