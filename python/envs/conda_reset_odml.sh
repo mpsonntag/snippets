a86f66880497ddfa25a948f05df4e2b7d0f024d5
M. Sonntag <michael.p.sonntag@gmail.com>
2018-05-22 20:11:33 +0200

    [python/envs] Simplify conda reset

--- python/envs/conda_reset_odml.sh
+++ python/envs/conda_reset_odml.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-CONDABIN=$HOME'/Chaos/software/mconda2/bin'
+CONDA=$HOME'/Chaos/software/mconda2/bin/conda'
 if [ $(which conda) ]; then
-    CONDABIN=$(which conda)"/bin"
+    CONDA=$(which conda)
 fi
+CONDABIN=${CONDA/"bin/conda"/"bin"}
+exit
 
 echo "-- Running odml_conda_deps_reset.sh"
-echo "-- Using conda at $CONDABIN"
+echo "-- Using conda at $CONDA"
 
 echo "-- make sure we are clean and not in an environment."
 source deactivate
 
 echo "-- Cleanup previous environments"
 
-$CONDABIN/conda remove -n o2 --all -y
-$CONDABIN/conda remove -n ot2 --all -y
-$CONDABIN/conda remove -n o3 --all -y
-$CONDABIN/conda remove -n ot3 --all -y
+$CONDA remove -n o2 --all -y
+$CONDA remove -n ot2 --all -y
+$CONDA remove -n o3 --all -y
+$CONDA remove -n ot3 --all -y
 
 echo "-- Create test environments"
 
-$CONDABIN/conda create -n o2 python=2.7 -y
-$CONDABIN/conda create -n ot2 python=2.7 -y
-$CONDABIN/conda create -n o3 python=3.5 -y
-$CONDABIN/conda create -n ot3 python=3.5 -y
+$CONDA create -n o2 python=2.7 -y
+$CONDA create -n ot2 python=2.7 -y
+$CONDA create -n o3 python=3.5 -y
+$CONDA create -n ot3 python=3.5 -y
 
 echo "-- Install dependencies"
 
@@ -43,5 +45,5 @@ conda install -c pkgw-forge adwaita-icon-theme -y
 
 echo "-- List current environments"
 
-$CONDABIN/conda info --envs
+$CONDA info --envs
 
