c9173a615f25e2d6e82e296d426cb84eee81fd94
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-24 15:00:16 +0200

    [py/convdebug] Add conversion

--- python/odml/debug_conversion.py
+++ python/odml/debug_conversion.py
@@ -6,10 +6,20 @@ from odml.tools.version_converter import VersionConverter as VC
 root = os.path.join(os.environ['HOME'], 'Chaos', 'DL', 'x_odml', 'convert')
 
 conv_no_enc = os.path.join(root, '1.3_convert.xml')
-conv_utf8 = os.path.join(root, '1.3_convert_utf8.xml')
-
 out_noenc = os.path.join(root, '1.4_converted_noenc.xml')
+
+conv_utf8 = os.path.join(root, '1.3_convert_utf8.xml')
 out_utf8 = os.path.join(root, '1.4_converted_utf8.xml')
 
-print("%s, %s, %s, %s" % (conv_no_enc, conv_utf8, out_noenc, out_utf8))
+# Convert no encoding
+conv = VC(conv_no_enc)
+conv.write_to_file(out_noenc)
 
+vdoc = odml.load(out_noenc)
+
+
+# Convert utf8 encoding in xml header
+conv = VC(conv_utf8)
+conv.write_to_file(out_utf8)
+
+vudoc = odml.load(out_utf8)
