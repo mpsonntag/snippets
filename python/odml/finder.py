be6aa0f9a7ac04f602b813d580d97731189cc8e1
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-30 18:43:43 +0200

    [pyfinder] Add and use version converter

--- python/odml/finder.py
+++ python/odml/finder.py
@@ -16,11 +16,13 @@ Options:
 """
 
 import os
+import odml
 import pathlib
 import sys
 import tempfile
 
 from docopt import docopt
+from odml.tools.version_converter import VersionConverter as VerConf
 from odml.tools.odmlparser import ODMLReader, ODMLWriter
 
 
@@ -29,6 +31,29 @@ def run_rdf_conversion(infile, dir):
     outfile = os.path.join(dir, "%s.rdf" % outname)
     doc = ODMLReader().from_file(infile)
     ODMLWriter("RDF").write_file(doc, outfile)
+
+
+def run_version_conversion(flist, convdir, rdir, source_backend="XML"):
+    for f in flist:
+        infile = str(f.absolute())
+        print("Handling file '%s'" % infile)
+        try:
+            odml.load(infile, source_backend)
+            print("RDF conversion of '%s'" % infile)
+            run_rdf_conversion(infile, rdir)
+        except Exception:
+            outname = os.path.splitext(os.path.basename(infile))[0]
+            outfile = os.path.join(convdir, "%s_conv.xml" % outname)
+            try:
+                _ = VerConf(infile).write_to_file(outfile, source_backend)
+                try:
+                    print("RDF conversion of '%s'" % outfile)
+                    run_rdf_conversion(outfile, rdir)
+                except Exception as e:
+                    print("[Error] converting '%s' to RDF: '%s'" % (infile, str(e)))
+            except Exception as e:
+                # Ignore files we cannot parse or convert
+                print("[Error] version converting file '%s': '%s'" % (infile, str(e)))
 
 
 def main(args=None):
@@ -58,6 +83,10 @@ def main(args=None):
     rdir = tempfile.mkdtemp(prefix="odmlrdf_", dir=tdir)
 
     print("Files will be saved to '%s'" % tdir)
+
+    run_version_conversion(xfiles, tdir, rdir)
+    run_version_conversion(jfiles, tdir, rdir, "JSON")
+    run_version_conversion(yfiles, tdir, rdir, "YAML")
 
 
 if __name__ == "__main__":
