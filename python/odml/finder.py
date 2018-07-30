446bce52cf4182a43ff0424557e4eec9de6d2369
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-30 16:18:32 +0200

    [py/finder] Update usage

--- python/odml/finder.py
+++ python/odml/finder.py
@@ -1,15 +1,18 @@
-"""FindConvert
+"""odmlFindConvert
 
-FindConvert searches for odml files within a provided SEARCHDIR
+odmlFindConvert searches for odml files within a provided SEARCHDIR
 and converts them a) to the newest odML format version and
 exports all found and resulting newest odML version files to RDF.
 
-Usage: odmlconv [-r] [SEARCHDIR]
+Usage: odmlconv [-r] SEARCHDIR
+
+Arguments:
+    SEARCHDIR       Directory to search for odML files.
 
 Options:
-  -r          search recursively.
-  -h --help   show this screen.
-  --version   show version.
+    -r              search recursively.
+    -h --help       show this screen.
+    --version       show version.
 """
 
 import os
