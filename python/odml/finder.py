2c9f2c33f720b96bf194bf0b2b41b6de11d7796b
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-30 15:50:23 +0200

    [py/finder] Add cli parser

---
+++ python/odml/finder.py
@@ -0,0 +1,31 @@
+"""FindConvert
+
+FindConvert searches for odml files within a provided SEARCHDIR
+and converts them a) to the newest odML format version and
+exports all found and resulting newest odML version files to RDF.
+
+Usage: odmlconv [-r] [SEARCHDIR]
+
+Options:
+  -r          search recursively.
+  -h --help   show this screen.
+  --version   show version.
+"""
+
+import os
+import sys
+
+from docopt import docopt
+
+
+class FindConvert(object):
+
+    @classmethod
+    def cli(cls, args=None):
+        parser = docopt(__doc__, argv=args, version="1.0.0")
+        print("%s, %s" % (parser['-r'], parser['SEARCHDIR']))
+
+
+if __name__ == "__main__":
+    FindConvert.cli(sys.argv[1:])
+
