661ea8d866b79e7ec2a32466248375d3a6e363a2
M. Sonntag <michael.p.sonntag@gmail.com>
2018-08-10 17:25:08 -0400

    [interlex] Make script callable

--- python/odml/example_interlex_load.py
+++ python/odml/example_interlex_load.py
@@ -1,13 +1,45 @@
+"""odmlInterLex
+
+odmlInterLex fetches all registered terms from scicrunch interlex
+for a given search term
+
+Usage: odmlilex SEARCHTERM
+
+Arguments:
+    SEARCHTERM       Term to get InterLex search terms.
+
+Options:
+    -h --help       show this screen.
+    --version       show version.
+"""
+
+import sys
+
+from docopt import docopt
 from urllib import request
 
-searchterm = "electrode"
-usrkey = "ZRt6argPOzoqCwk8ULK5N7agk731VsZy"
 
-resp = request.urlopen("https://scicrunch.org/api/1/ilx/search/term/%s?key=%s" %
-                       (searchterm, usrkey))
+def main(args=None):
 
-print("Status response: '%s'; length: %s" % (resp.msg, resp.length()))
+    print("bla")
 
-content = resp.read()
+    parser = docopt(__doc__, argv=args, version="0.1.0")
 
-print("Content:\n%s\n" % content)
+    searchterm = parser['SEARCHTERM']
+    usrkey = "ZRt6argPOzoqCwk8ULK5N7agk731VsZy"
+
+    requ = "https://scicrunch.org/api/1/ilx/search/term/%s?key=%s" % (searchterm, usrkey)
+
+    print("Running '%s'" % requ)
+
+    resp = request.urlopen(requ)
+
+    print("Status response: '%s'; length: %s" % (resp.msg, resp.status))
+
+    content = resp.read()
+
+    print("Content:\n%s\n" % content)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
