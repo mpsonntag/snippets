e851a50461221bce20e19a6abecfe636607106c6
M. Sonntag <michael.p.sonntag@gmail.com>
2018-08-23 16:32:28 +0200

    [graphpush/config] Update log settings

--- cmd/graphpush/config/config.go
+++ cmd/graphpush/config/config.go
@@ -7,21 +7,16 @@ import (
 
 // Directories used by the server for temporary and long term storage.
 type Directories struct {
+	Temp   string `json:"temp"`
 	Source string `json:"source"`
 	Log    string `json:"log"`
 }
 
-// Denotations provide any freuquently used file names or other denotations
-// e.g. validation result files, badge or result folder names.
-type Denotations struct {
-	LogFile string `json:"logfile"`
-}
-
 // Settings provide the default server settings.
-// "Validators" currently only supports "BIDS".
 type Settings struct {
 	Port     string `json:"port"`
 	LogSize  int    `json:"logsize"`
+	LogFile  string `json:"logfile"`
 	MetaUser string `json:"metauser"`
 	MetaPW   string `json:"metapw"`
 }
@@ -31,20 +26,17 @@ type Settings struct {
 type ServerCfg struct {
 	Settings Settings    `json:"settings"`
 	Dir      Directories `json:"directories"`
-	Label    Denotations `json:"denotations"`
 }
 
 var defaultServer = ServerCfg{
 	Settings{
 		Port:    "6066",
 		LogSize: 1048576,
+		LogFile: "graphpush.log",
 	},
 	Directories{
 		Source: filepath.Join(os.Getenv("GRAPHPUSHSOURCE")),
 		Log:    filepath.Join(os.Getenv("GRAPHPUSHHOME")),
-	},
-	Denotations{
-		LogFile: "graphpush.log",
 	},
 }
 
