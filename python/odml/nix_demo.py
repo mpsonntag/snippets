498cd1e704aa18c8b2c95e87a97b879f26965687
M. Sonntag <michael.p.sonntag@gmail.com>
2018-11-04 13:28:52 -0800

    [python/odml] nix demo: add data array to nix file

--- python/odml/nix_demo.py
+++ python/odml/nix_demo.py
@@ -1,20 +1,37 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-fn = "/Users/michi/Chaos/work/_Lab_Zimmer/calcium_imaging/results/N2/urx/shift210421/20120705Pflp178GCaMP5kshift210421W7URXx2.log"
+from pathlib import Path
+
+import nixio as nix
+
+fnbase = Path.joinpath(Path.home(), 'Chaos', 'work')
+
+
+fnraw = str(Path.joinpath(fnbase,
+                          '_Lab_Zimmer/calcium_imaging/results/N2/urx/shift210421/20120705Pflp178GCaMP5kshift210421W7URXx2.log'))
+
+nixfn = str(Path.joinpath(fnbase, 'ginrepos', 'demo', 'elegans_oxygen.nix'))
+
 
 # row_wise read in of csv file
-data = pd.read_csv(fn)
+data = pd.read_csv(fnraw)
 
 # transpose to get columns
 tdata = data.transpose()
 
 # get df/f column as array
+steps = tdata.values[0]
 col = tdata.values[5]
 
-fig, ax = plt.subplots()
-ax.plot(col)
-ax.set(xlabel='frames', ylabel='dF/F', title="URX oxygen shift trial")
+# load data into nix
+nixfile = nix.File.open(nixfn, nix.FileMode.Overwrite)
+b = nixfile.create_block(name="oxygen_shift_trials", type_="calcium_imaging")
+da = b.create_data_array(name="N2_URX_shift_210421_20120705",
+                         array_type="df_over_f", data=col)
+da.label = "dF/F"
 
-plt.show()
+# Add the second dimension to the data array
+dim = da.append_sampled_dimension(steps[1] - steps[0])
+dim.label = "frames"
 
