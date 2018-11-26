0d1f57c7208d9f7bfb31b33361dd25a8a0bed990
M. Sonntag <michael.p.sonntag@gmail.com>
2018-11-26 15:48:50 +0100

    [py/env] Cleanup odmlui test pip install

--- python/envs/odmlui_pypi_test_install.sh
+++ python/envs/odmlui_pypi_test_install.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+echo "-- Running odmlui_pypi_test_install.sh"
+echo "-- Using pip at $(which pip)"
+
 pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple -I odml-ui
 
