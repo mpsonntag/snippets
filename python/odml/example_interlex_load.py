e6d1eb6b635f3f379470217f25c0270e397563a3
M. Sonntag <michael.p.sonntag@gmail.com>
2019-03-23 12:21:00 +0100

    [py/odml] SciCrunch Add subfunctions

--- python/odml/example_interlex_load.py
+++ python/odml/example_interlex_load.py
@@ -37,6 +37,42 @@ SCICRUNCH_USERKEY = "ZRt6argPOzoqCwk8ULK5N7agk731VsZy"
 SCICRUNCH_URI = "https://scicrunch.org/api/1"
 
 
+def search_interlex(search_term):
+    requ = "%s/ilx/search/term/%s?key=%s" % (SCICRUNCH_URI, search_term, SCICRUNCH_USERKEY)
+
+    print("Running interlex query '%s'" % requ)
+
+    resp = urllib2.urlopen(requ)
+
+    if resp.getcode() != httplib.OK:
+        print("Could not resolve query, exit with '%s/%s'" % (resp.msg, resp.getcode()))
+        exit(-1)
+
+    content = json.load(resp)
+
+    print("Result: '%s'" % content)
+
+    data = None
+    try:
+        data = content["data"]
+    except KeyError:
+        print("Received invalid response '%s'" % content)
+        exit(-1)
+
+    if not data["id"]:
+        print("Search returned no result")
+        exit(0)
+
+    print("InterlexID: '%s'" % data["ilx"])
+    print("Label: '%s'" % data["label"])
+    print("Type: '%s'" % data["type"])
+    print("definition: '%s'" % data["definition"])
+    print("Number of available ids: '%s'" % len(data["existing_ids"]))
+
+    for res in data["existing_ids"]:
+        print("Current item: '%s'" % res)
+
+
 def main(args=None):
     # Find more details at
     # https://github.com/SciCrunch/NIF-Ontology
@@ -44,50 +80,19 @@ def main(args=None):
 
     parser = docopt(__doc__, argv=args, version="0.1.0")
 
-    searchtype = parser['SEARCHTYPE']
-    searchterm = parser['SEARCHTERM']
+    search_type = parser['SEARCHTYPE']
+    search_term = parser['SEARCHTERM']
 
-    if searchtype == "1":
-        requ = "%s/ilx/search/term/%s?key=%s" % (SCICRUNCH_URI, searchterm, SCICRUNCH_USERKEY)
+    if search_type == "1":
+        search_interlex(search_term)
 
-        print("Running interlex query '%s'" % requ)
-
-        resp = urllib2.urlopen(requ)
-
-        if resp.getcode() != httplib.OK:
-            print("Could not resolve query, exit with '%s/%s'" % (resp.msg, resp.getcode()))
-            exit(-1)
-
-        content = json.load(resp)
-
-        print("Result: '%s'" % content)
-
-        data = None
-        try:
-            data = content["data"]
-        except KeyError:
-            print("Received invalid response '%s'" % content)
-            exit(-1)
-
-        if not data["id"]:
-            print("Search returned no result")
-            exit(0)
-
-        print("InterlexID: '%s'" % data["ilx"])
-        print("Label: '%s'" % data["label"])
-        print("Type: '%s'" % data["type"])
-        print("definition: '%s'" % data["definition"])
-        print("Number of available ids: '%s'" % len(data["existing_ids"]))
-
-        for res in data["existing_ids"]:
-            print("Current item: '%s'" % res)
-    elif searchtype == "2":
+    elif search_type == "2":
         pass
-    elif searchtype == "3":
+    elif search_type == "3":
         pass
     else:
-        print("Unsupported search type, aborting.")
-        exit(-1)
+        print("WARNING: Unsupported search type, defaulting to InterLex search.")
+        search_interlex(search_term)
 
 
 if __name__ == "__main__":
