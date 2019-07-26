15f569dae823d02aab14f89453f2e16883a837de
M. Sonntag <michael.p.sonntag@gmail.com>
2019-07-26 18:34:23 +0200

    [python/servem] Add odml extension support

--- python/servodml/servem.py
+++ python/servodml/servem.py
@@ -4,6 +4,8 @@ import socketserver
 
 def run(directory=None, port=8000):
     handler = hs.SimpleHTTPRequestHandler
+    # files with odML extensions should be interpreted as XML
+    handler.extensions_map.update({'.odml': 'application/xml'})
     server_address = ('', port)
 
     with socketserver.TCPServer(server_address, handler) as httpd:
