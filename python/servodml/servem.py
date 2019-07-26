292a5b4dce1bfed822cf6fa6eefce4ad58a1060d
M. Sonntag <michael.p.sonntag@gmail.com>
2019-07-26 19:49:25 +0200

    [python/servem] Add command line arguments

--- python/servodml/servem.py
+++ python/servodml/servem.py
@@ -1,5 +1,24 @@
+"""serveOdml
+
+serverLocalOdml sets up a minimal webserver and serves
+and renders local odml files.
+
+Usage: serveOdml [-d DIRECTORY] [-p PORT]
+
+Options:
+    -d DIRECTORY    Directory from which files will be served
+                    Default is the directory from which the server is started.
+    -p PORT         Port the server will use.
+                    Default is port 8000.
+    -h --help       Show this screen.
+    --version       Show version.
+"""
+
 import http.server as hs
 import socketserver
+import sys
+
+from docopt import docopt
 
 
 def run(directory=None, port=8000):
@@ -13,4 +32,9 @@ def run(directory=None, port=8000):
 
 
 if __name__ == "__main__":
-    run()
+    parser = docopt(__doc__, argv=sys.argv[1:], version="0.1.0")
+
+    server_port = parser['-p'] if parser['-p'] else 8000
+    serve_directory = parser['-d'] if parser['-d'] else None
+
+    run(serve_directory, server_port)
