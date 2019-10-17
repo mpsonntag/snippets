e42c42373a7e0494f9f81f25e264da4072ad6cec
M. Sonntag <michael.p.sonntag@gmail.com>
2019-10-17 13:48:49 +0200

    [py/doi->odml/conv] Add cli parser

--- python/doiToOdml/datacite_to_odml.py
+++ python/doiToOdml/datacite_to_odml.py
@@ -1,12 +1,34 @@
-"""
-Convert a datacite xml file to odml.
+"""dataciteToOdml
+
+Convenience script to parse a datacite xml file
+and create a new odML with the parsed information.
+
+Usage: datacitetoodml FILENAME
+
+Arguments:
+    FILENAME    Path and filename of the datacite xml file to be parsed.
+
+Options:
+    -h --help   Show this screen.
+    --version   Show version number.
+
 """
 
+import os
 import sys
+
+from docopt import docopt
+
+VERSION = "0.1.0"
 
 
 def main(args=None):
-    print("Converting datacite to odml")
+    parser = docopt(__doc__, argv=args, version=VERSION)
+
+    cite_file = parser['FILENAME']
+    if not os.path.isfile(cite_file):
+        print("[Error] Could not access file '%s'\n" % cite_file)
+        exit(1)
 
 
 if __name__ == "__main__":
