3655e58dbec1e2892fcf430e5269791bb4760d36
M. Sonntag <michael.p.sonntag@gmail.com>
2019-10-17 15:32:41 +0200

    [py/doi->odml/conv] Parse xml file

--- python/doiToOdml/odml_from_datacite.py
+++ python/doiToOdml/odml_from_datacite.py
@@ -17,8 +17,15 @@ import os
 import sys
 
 from docopt import docopt
+from lxml import etree as ET
+from lxml.etree import XMLSyntaxError
 
 VERSION = "0.1.0"
+
+
+def parse_datacite(xml_file):
+    parser = ET.XMLParser(remove_comments=True)
+    ET.parse(xml_file, parser).getroot()
 
 
 def main(args=None):
@@ -26,7 +33,14 @@ def main(args=None):
 
     cite_file = parser['FILENAME']
     if not os.path.isfile(cite_file):
-        print("[Error] Could not access file '%s'\n" % cite_file)
+        print("[Error] Could not access input file '%s'\n" % cite_file)
+        exit(1)
+
+    try:
+        parse_datacite(cite_file)
+    except XMLSyntaxError as exc:
+        print("[Error] Could not parse input file '%s'" % cite_file)
+        print("\t%s" % exc.msg)
         exit(1)
 
 
