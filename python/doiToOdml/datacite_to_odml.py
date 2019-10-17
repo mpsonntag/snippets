419c3ed0d57fdbf06f1baa52ad16214fc6c60b31
M. Sonntag <michael.p.sonntag@gmail.com>
2019-10-17 13:28:46 +0200

    [py/doi->odml] Add conversion script file

---
+++ python/doiToOdml/datacite_to_odml.py
@@ -0,0 +1,13 @@
+"""
+Convert a datacite xml file to odml.
+"""
+
+import sys
+
+
+def main(args=None):
+    print("Converting datacite to odml")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
