465c5f6651804da2c9317706a8d1aff0d5e67fae
M. Sonntag <michael.p.sonntag@gmail.com>
2019-10-24 18:03:38 +0200

    [py/doi->odml/conv] Cleanup script

--- python/doiToOdml/odml_from_datacite.py
+++ python/doiToOdml/odml_from_datacite.py
@@ -16,13 +16,13 @@ Options:
 import os
 import re
 import sys
+
+from xml.parsers.expat import ExpatError
+
 import xmltodict
 
 from docopt import docopt
-from xml.parsers.expat import ExpatError
-
-import odml
-
+from odml import Document, Section, Property
 from odml.dtypes import DType
 
 
@@ -45,9 +45,9 @@ class DataCiteItem(object):
         self.item_func = item_func
 
 
-def camel_to_snake(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+def camel_to_snake(in_string):
+    tmp = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', in_string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', tmp).lower()
 
 
 def dict_from_xml(xml_file):
@@ -61,7 +61,7 @@ def dict_from_xml(xml_file):
     try:
         with open(xml_file) as file:
             doc = xmltodict.parse(file.read())
-    except ExpatError as exc:
+    except ExpatError:
         raise ParserException("Could not parse file")
 
     return doc
@@ -74,9 +74,7 @@ def handle_container(helper, node, root_sec):
     sec_cont_type = "datacite/%s" % camel_to_snake(helper.container_name)
     sub_sec_type = "datacite/%s" % camel_to_snake(helper.section_name)
 
-    sec = odml.Section(name=helper.container_name,
-                       type=sec_cont_type,
-                       parent=root_sec)
+    sec = Section(name=helper.container_name, type=sec_cont_type, parent=root_sec)
 
     handle_sub_container(helper, node, sec, sub_sec_type)
 
@@ -89,14 +87,11 @@ def handle_sub_container(helper, node, sec, sub_sec_type):
     if isinstance(node[helper.section_name], list):
         for (idx, title_node) in enumerate(node[helper.section_name]):
             sec_name = "%s_%d" % (helper.section_name, idx + 1)
-            sub_sec = odml.Section(name=sec_name,
-                                   type=sub_sec_type,
-                                   parent=sec)
+            sub_sec = Section(name=sec_name, type=sub_sec_type, parent=sec)
             helper.item_func(helper, title_node, sub_sec)
     else:
-        sub_sec = odml.Section(name="%s_1" % helper.section_name,
-                               type=sub_sec_type,
-                               parent=sec)
+        sub_sec_name = "%s_1" % helper.section_name
+        sub_sec = Section(name=sub_sec_name, type=sub_sec_type, parent=sec)
         helper.item_func(helper, node[helper.section_name], sub_sec)
 
 
@@ -105,9 +100,7 @@ def handle_sec(helper, node, root_sec):
         return
 
     sec_type = "datacite/%s" % camel_to_snake(helper.section_name)
-    sec = odml.Section(name=helper.section_name,
-                       type=sec_type,
-                       parent=root_sec)
+    sec = Section(name=helper.section_name, type=sec_type, parent=root_sec)
 
     handle_props(helper, node, sec)
 
@@ -118,7 +111,7 @@ def handle_props(helper, node, sec):
 
     # Handle special case if a node is just the string content of an XML element.
     if isinstance(node, str):
-        odml.Property(name=helper.section_name, values=node, parent=sec)
+        Property(name=helper.section_name, values=node, parent=sec)
     else:
         for sub in node:
             if sub in helper.attribute_map:
@@ -126,8 +119,8 @@ def handle_props(helper, node, sec):
                 if isinstance(sub, str) and sub.endswith("URI"):
                     dtype = DType.url
 
-                odml.Property(name=helper.attribute_map[sub], dtype=dtype,
-                              values=node[sub], parent=sec)
+                Property(name=helper.attribute_map[sub], dtype=dtype,
+                         values=node[sub], parent=sec)
             else:
                 print("[Warning] Ignoring node '%s/%s'" % (sec.name, sub))
 
@@ -176,9 +169,9 @@ def handle_creators_item(_, node, sec):
             handle_props(creator_name_helper, node[sub], sec)
         elif sub in ["givenName", "familyName"]:
             if isinstance(node[sub], str):
-                odml.Property(name=sub, values=node[sub], parent=sec)
+                Property(name=sub, values=node[sub], parent=sec)
             elif "#text" in node[sub]:
-                odml.Property(name=sub, values=node[sub]["#text"], parent=sec)
+                Property(name=sub, values=node[sub]["#text"], parent=sec)
             else:
                 print("[Warning] Could not parse '%s/%s'" % (sub_type_base, sub))
         elif sub == "nameIdentifier":
@@ -195,13 +188,13 @@ def handle_contributors_item(_, node, sec):
     for sub in node:
         if sub in ["contributorName", "givenName", "familyName"]:
             if isinstance(node[sub], str):
-                odml.Property(name=sub, values=node[sub], parent=sec)
+                Property(name=sub, values=node[sub], parent=sec)
             elif "#text" in node[sub]:
-                odml.Property(name=sub, values=node[sub]["#text"], parent=sec)
+                Property(name=sub, values=node[sub]["#text"], parent=sec)
             else:
                 print("[Warning] Could not parse '%s/%s'" % (sub_type_base, sub))
         elif sub == "@contributorType":
-            odml.Property(name="contributorType", values=node[sub], parent=sec)
+            Property(name="contributorType", values=node[sub], parent=sec)
         elif sub == "nameIdentifier":
             handle_name_identifiers(sub, node, sub_type_base, sec)
         elif sub == "affiliation":
@@ -211,13 +204,13 @@ def handle_contributors_item(_, node, sec):
 
 
 def handle_geo_entry(helper_list, node, sec, sub_sec_name, sub_sec_type):
-    sub_sec = odml.Section(name=sub_sec_name, type=sub_sec_type, parent=sec)
+    sub_sec = Section(name=sub_sec_name, type=sub_sec_type, parent=sec)
 
     for entry in node:
         if entry in helper_list:
             try:
-                odml.Property(name=entry, dtype=DType.float,
-                              values=node[entry], parent=sub_sec)
+                Property(name=entry, dtype=DType.float,
+                         values=node[entry], parent=sub_sec)
             except ValueError:
                 print("[Warning] Skipping invalid '%s' value '%s'" %
                       (entry, node[entry]))
@@ -232,7 +225,7 @@ def handle_geo_locations(_, node, sec):
 
     for elem in node:
         if elem == "geoLocationPlace":
-            odml.Property(name=elem, values=node[elem], parent=sec)
+            Property(name=elem, values=node[elem], parent=sec)
         elif elem == "geoLocationPoint":
             sec_type = "%s/%s" % (sub_type_base, camel_to_snake(elem))
             handle_geo_entry(point_list, node[elem], sec, elem, sec_type)
@@ -241,7 +234,7 @@ def handle_geo_locations(_, node, sec):
             handle_geo_entry(box_list, node[elem], sec, elem, sec_type)
         elif elem == "geoLocationPolygon":
             sub_type = "%s/%s" % (sub_type_base, camel_to_snake(elem))
-            sub_sec = odml.Section(name=elem, type=sub_type, parent=sec)
+            sub_sec = Section(name=elem, type=sub_type, parent=sec)
 
             for (idx, point) in enumerate(node[elem]["polygonPoint"]):
                 point_name = "polygonPoint_%d" % (idx + 1)
@@ -252,7 +245,7 @@ def handle_geo_locations(_, node, sec):
 def handle_funding_references(_, node, sec):
     for sub in node:
         if sub in ["funderName", "awardTitle"]:
-            odml.Property(name=sub, values=node[sub], parent=sec)
+            Property(name=sub, values=node[sub], parent=sec)
         elif sub == "awardNumber":
             award_number_map = {"#text": "awardNumber", "@awardURI": "awardURI"}
             award_number_helper = DataCiteItem(sec_name=sub,
@@ -452,10 +445,10 @@ def parse_datacite_dict(doc):
         "fundingReferences": funding_references_helper
     }
 
-    odml_doc = odml.Document()
+    odml_doc = Document()
     odml_doc.repository = "https://terminologies.g-node.org/v1.1/terminologies.xml"
 
-    root_sec = odml.Section(name="DataCite", type="data_reference", parent=odml_doc)
+    root_sec = Section(name="DataCite", type="data_reference", parent=odml_doc)
 
     for node_tag in datacite_root:
         if node_tag in supported_tags:
@@ -466,7 +459,7 @@ def parse_datacite_dict(doc):
 
     # ToDo make prints optional
     print()
-    print(odml_doc.pprint(max_depth=2))
+    print(odml_doc.pprint(max_depth=5))
 
     # ToDo handle file saving
 
