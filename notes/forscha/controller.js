6d04fbe507c9bcbf755402877194f7e5078387b6
M. Sonntag <michael.p.sonntag@gmail.com>
2019-10-28 21:16:58 +0100

    [notes/forscha] Use german

--- notes/forscha/controller.js
+++ notes/forscha/controller.js
@@ -121,22 +121,22 @@ function displayQuestion(){
       var q2;
       var q3;
 
-      if(rnd==1){q1=questionBank[questionNumber][1]; q2=questionBank[questionNumber][2]; q3=questionBank[questionNumber][3];}1
+      if(rnd==1){q1=questionBank[questionNumber][1]; q2=questionBank[questionNumber][2]; q3=questionBank[questionNumber][3];}
       if(rnd==2){q2=questionBank[questionNumber][1]; q3=questionBank[questionNumber][2]; q1=questionBank[questionNumber][3];}
       if(rnd==3){q3=questionBank[questionNumber][1]; q1=questionBank[questionNumber][2]; q2=questionBank[questionNumber][3];}
 
-    $(stage).append('<div class = "questionText">' + questionBank[questionNumber][0] + '</div><div id= "1" class="option">'+q1+'</div> <div id="2" class="option">'+q2+'</div> <div id="3" class="option">'+q3+'</div>');
+    $(stage).append('<div class = "questionText">'+questionBank[questionNumber][0]+'</div><div id= "1" class="option">'+q1+'</div> <div id="2" class="option">'+q2+'</div> <div id="3" class="option">'+q3+'</div>');
 
     $('.option').click(function(){
       if(questionLock==false){questionLock=true;
       //correct answer
       if(this.id==rnd){
-      $(stage).append('<div class="feedback1">CORRECT</div>');
+      $(stage).append('<div class="feedback1">RICHTIG</div>');
       score++;
       }
       //wrong answer
       if(this.id!=rnd){
-      $(stage).append('<div class="feedback2">WRONG</div>');
+      $(stage).append('<div class="feedback2">LEIDER NEIN</div>');
       }
       setTimeout(function(){changeQuestion()},1000);
       }})
@@ -162,7 +162,7 @@ function changeQuestion(){
 
 function displayFinalSlide(){
 
-    $(stage).append(" <div class='questionText' >You have finished the quiz!<br><br>Total questions: "+numberOfQuestions+"<br>Correct answers: "+score+"</div>");
+    $(stage).append(" <div class='questionText'>Gratuliere, du hast das Quiz geschafft!<br><br>Anzahl Fragen: "+numberOfQuestions+"<br>Richtige Antworten: "+score+"</div>");
 
 }//display final slide
 });
