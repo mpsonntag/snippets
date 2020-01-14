d8f0f505f2bd2891e4e64c1fc0e59b02845d793d
M. Sonntag <michael.p.sonntag@gmail.com>
2020-01-14 19:29:12 +0100

    [py/doi->odml] Cleanup query fetch

--- python/odmlFromDatacite/fetch_datacite.py
+++ python/odmlFromDatacite/fetch_datacite.py
@@ -4,33 +4,40 @@ import requests
 
 import datacite
 
+
 # Adjust as required
-output_dir = os.path.join(os.sep, "tmp", "datacite")
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+OUTPUT_DIR = os.path.join(os.sep, "tmp", "datacite")
+DATACITE_API_ENDPOINT = "https://api.datacite.org/dois"
+DATACITE_QUERY = "?query=publisher:CRCNS.org"
 
-datacite_api_endpoint = "https://api.datacite.org/dois"
-datacite_query = "query=publisher:CRCNS.org&fl=doi,minted,updated,xml&fq=has_metadata:true&fq=is_active:true&rows=1000&start=0&sort=updated+asc&wt=json"
 
-datacite_uri = "%s?%s" % (datacite_api_endpoint, datacite_query)
+def fetch_datacite():
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
-print("... running DataCite query")
-req = requests.get(datacite_uri)
-req_json = json.loads(req.text.encode("utf-8"))
+    datacite_uri = "%s%s" % (DATACITE_API_ENDPOINT, DATACITE_QUERY)
 
-datacite_items = req_json["data"]
-print("... query returned %s data-sets" % len(datacite_items))
+    print("... running DataCite query")
+    req = requests.get(datacite_uri)
+    req_json = json.loads(req.text.encode("utf-8"))
 
-# DataCite interaction client without authorization;
-# used to fetch DataCite XML metadata for specific DOIs
-dclient = datacite.DataCiteMDSClient()
+    datacite_items = req_json["data"]
+    print("... query returned %s data-sets" % len(datacite_items))
 
-for idx, datacite_item in enumerate(datacite_items):
-    curr_doi = datacite_items[idx]['attributes']['doi']
-    curr_filename = "%s.xml" % curr_doi.split("/")[1]
-    curr_file_path = os.path.join(output_dir, curr_filename)
+    # DataCite interaction client without authorization;
+    # used to fetch DataCite XML metadata for specific DOIs
+    dclient = datacite.DataCiteMDSClient()
 
-    print("... fetching DataCite file '%s'" % curr_filename)
-    curr_xml = dclient.metadata_get(curr_doi)
-    with open(curr_file_path, "w+") as curr_file:
-        curr_file.write(curr_xml)
+    for datacite_item in datacite_items:
+        curr_doi = datacite_item['attributes']['doi']
+        curr_filename = "%s.xml" % curr_doi.split("/")[1]
+        curr_file_path = os.path.join(OUTPUT_DIR, curr_filename)
+
+        print("... fetching DataCite file '%s'" % curr_filename)
+        curr_xml = dclient.metadata_get(curr_doi)
+        with open(curr_file_path, "w+") as curr_file:
+            curr_file.write(curr_xml)
+
+
+if __name__ == "__main__":
+    fetch_datacite()
