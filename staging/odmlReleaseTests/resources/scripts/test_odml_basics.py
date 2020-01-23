038351a963a444d3d2f688d05a94a5f421dcc2ae
M. Sonntag <michael.p.sonntag@gmail.com>
2020-01-23 15:04:45 +0100

    [staging/odmlRelease] Update basic tests script

--- staging/odmlReleaseTests/resources/scripts/test_odml_basics.py
+++ staging/odmlReleaseTests/resources/scripts/test_odml_basics.py
@@ -1,16 +1,36 @@
-# Test possible imports of all parsers without importing the full odML package
-from odml.tools import ODMLReader, ODMLWriter, RDFReader, RDFWriter
-from odml.tools.converters import FormatConverter, VersionConverter
-from odml.tools import XMLReader, XMLWriter, DictReader, DictWriter
-import odml
+import sys
+import unittest
 
-print("\nLoading odml document")
-doc = odml.load('./load.odml.xml')
-
-doc.pprint()
-
-print("\nLoading previous version odml document")
 try:
-    doc = odml.load('./load_v1.odml.xml')
-except odml.tools.parser_utils.InvalidVersionException as exc:
-    print("\n Could not load file: %s" % exc)
+    # Test possible imports of all parsers without importing the full odML package
+    from odml.tools import ODMLReader, ODMLWriter, RDFReader, RDFWriter
+    from odml.tools.converters import FormatConverter, VersionConverter
+    from odml.tools import XMLReader, XMLWriter, DictReader, DictWriter
+    import odml
+except Exception:
+    print("-- Failed on an import")
+    sys.exit(-1)
+
+
+class TestODMLBasics(unittest.TestCase):
+
+    def testLoad(self):
+        print("-- Load odml xml file")
+        doc = odml.load('./load.odml.xml')
+        print(doc.pprint())
+
+    def testVersionLoad(self):
+        print("-- Test invalid version exception xml file load")
+        with self.assertRaises(odml.tools.parser_utils.InvalidVersionException):
+            _ = odml.load('./load_v1.odml.xml')
+
+
+if __name__ == "__main__":
+    try:
+        test = TestODMLBasics()
+        test.testLoad()
+        test.testVersionLoad()
+
+    except Exception:
+        print("-- Failed on a test")
+        sys.exit(-1)
