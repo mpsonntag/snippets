a78c187869658f5628e96b22549f97bf79f06306
M. Sonntag <michael.p.sonntag@gmail.com>
2020-01-24 15:14:49 +0100

    [staging/odmlRelease] Refactor local test matrix

--- staging/odmlReleaseTests/run_local_tests_matrix.sh
+++ staging/odmlReleaseTests/run_local_tests_matrix.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 echo
-echo "-- MAKE SURE TO RUN THIS SCRIPT IN INTERACTIVE MODE '-i' --"
+echo "-- Running local python-odml test matrix"
 
 ROOT_DIR=$(pwd)
+LOG_DIR=/tmp/odml/local_test
+SCRIPT=./run_local_tests.sh
 
 echo
 echo "-- Running directory check: ${ROOT_DIR}"
@@ -14,40 +16,62 @@ if [[ ! "${CHECK_DIR}" = "odmlReleaseTests" ]]; then
     exit 1
 fi
 
-LOG_DIR=/tmp/local_test_odml
+echo
+echo "-- Creating log directory ${LOG_DIR}"
 mkdir -vp ${LOG_DIR}
 if [[ ! -d "${LOG_DIR}" ]]; then
     echo
     echo "-- Cannot find ${LOG_DIR} output directory"
-    echo
     exit 1
 fi
 
-SCRIPT=./run_local_tests.sh
+echo
+echo "-- Log files of all tests can be found in ${LOG_DIR}"
 
 PYVER=3.8
 echo
 echo "-- Running script for Python version ${PYVER}"
 bash -i ${SCRIPT} ${PYVER} > ${LOG_DIR}/${PYVER}_testrun.log 2>&1
+FAIL_COUNT=$(cat ${LOG_DIR}/${PYVER}_testrun.log | grep -c FAILED)
+if [[ "${FAIL_COUNT}" -gt 0 ]]; then
+    echo "-- Test fail in Python ${PYVER} tests. Check ${LOG_DIR}/${PYVER}_testrun.log"
+fi
 
 PYVER=3.7
 echo
 echo "-- Running script for Python version ${PYVER}"
 bash -i ${SCRIPT} ${PYVER} > ${LOG_DIR}/${PYVER}_testrun.log 2>&1
+FAIL_COUNT=$(cat ${LOG_DIR}/${PYVER}_testrun.log | grep -c FAILED)
+if [[ "${FAIL_COUNT}" -gt 0 ]]; then
+    echo "-- Test fail in Python ${PYVER} tests. Check ${LOG_DIR}/${PYVER}_testrun.log"
+fi
 
 PYVER=3.6
 echo
 echo "-- Running script for Python version ${PYVER}"
 bash -i ${SCRIPT} ${PYVER} > ${LOG_DIR}/${PYVER}_testrun.log 2>&1
+FAIL_COUNT=$(cat ${LOG_DIR}/${PYVER}_testrun.log | grep -c FAILED)
+if [[ "${FAIL_COUNT}" -gt 0 ]]; then
+    echo "-- Test fail in Python ${PYVER} tests. Check ${LOG_DIR}/${PYVER}_testrun.log"
+fi
 
 PYVER=3.5
 echo
 echo "-- Running script for Python version ${PYVER}"
 bash -i ${SCRIPT} ${PYVER} > ${LOG_DIR}/${PYVER}_testrun.log 2>&1
+FAIL_COUNT=$(cat ${LOG_DIR}/${PYVER}_testrun.log | grep -c FAILED)
+if [[ "${FAIL_COUNT}" -gt 0 ]]; then
+    echo "-- Test fail in Python ${PYVER} tests. Check ${LOG_DIR}/${PYVER}_testrun.log"
+fi
 
 PYVER=2.7
 echo
 echo "-- Running script for Python version ${PYVER}"
 bash -i ${SCRIPT} ${PYVER} > ${LOG_DIR}/${PYVER}_testrun.log 2>&1
+FAIL_COUNT=$(cat ${LOG_DIR}/${PYVER}_testrun.log | grep -c FAILED)
+if [[ "${FAIL_COUNT}" -gt 0 ]]; then
+    echo "-- Test fail in Python ${PYVER} tests. Check ${LOG_DIR}/${PYVER}_testrun.log"
+fi
 
+echo
 echo "-- Done"
