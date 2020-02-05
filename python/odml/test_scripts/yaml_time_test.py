572b8b9204323b5ee26c37e9265107bf3f9ec382
M. Sonntag <michael.p.sonntag@gmail.com>
2020-02-05 17:14:45 +0100

    [py/odml] Update yaml time test

--- python/odml/test_scripts/yaml_time_test.py
+++ python/odml/test_scripts/yaml_time_test.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import os
 
 import odml
 
@@ -7,18 +8,25 @@ from odml.tools import ODMLReader
 doc = odml.Document()
 sec = odml.Section(name="name", parent=doc)
 
-time_string = '12:34:56'
+time_string = "12:34:56"
 time = dt.time(12, 34, 56)
 val_in = time_string
 vals_in = [None, "", time_string, time]
 _ = odml.Property(name="time test single", dtype="time", value=val_in, parent=sec)
 _ = odml.Property(name="time test", dtype="time", value=vals_in, parent=sec)
 
-fname = '/home/msonntag/Chaos/DL/odmltestsave.YAML'
-odml.save(doc, fname, 'YAML')
+fdir = "/home/msonntag/Chaos/DL/tmp/odml"
 
-ydoc = odml.load(fname, 'YAML')
+if not os.path.exists(fdir):
+    os.makedirs(fdir)
 
+fname = "odmltestsave.YAML"
+fpath = os.path.join(fdir, fname)
+
+odml.save(doc, fpath, "YAML")
+
+ydoc = odml.load(fpath, "YAML")
+ydoc.pprint()
 
 author = "HPL"
 date = "1890-08-20"
@@ -26,7 +34,7 @@ sec_name = "section name"
 sec_type = "section type"
 prop_name = "prop name"
 prop_type = "time"
-prop_value = '15:15:15'
+prop_value = "15:15:15"
 yaml_doc = """
         odml-version: '1.1'
         Document:
@@ -42,4 +50,5 @@ yaml_doc = """
                 - %s
         """ % (author, date, sec_name, sec_type, prop_name, prop_type, prop_value)
 
-test = ODMLReader('YAML').from_string(yaml_doc)
+test = ODMLReader("YAML").from_string(yaml_doc)
+test.pprint()
