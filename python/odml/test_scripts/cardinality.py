d8ca76eefa27463427aff904f65eb6082e4915aa
M. Sonntag <michael.p.sonntag@gmail.com>
2020-03-30 21:33:33 +0200

    [py/odml] Add cardinality assignment test

--- python/odml/test_scripts/cardinality.py
+++ python/odml/test_scripts/cardinality.py
@@ -2,4 +2,19 @@ import odml
 
 doc = odml.Document()
 sec = odml.Section(name="sec", parent=doc)
-prop = odml.Property(name="prop", val_cardinality=10, parent=sec)
+
+# -- Test set cardinality on Property init
+# Test empty init
+prop_card_none = odml.Property(name="prop_cardinality_empty", parent=sec)
+assert prop_card_none.val_cardinality is None
+prop_card_none.val_cardinality
+
+# Test single int max init
+prop_card_max = odml.Property(name="prop_cardinality_max", val_cardinality=10, parent=sec)
+assert prop_card_max.val_cardinality == (None, 10)
+prop_card_max.val_cardinality
+
+# Test tuple init
+prop_card_min = odml.Property(name="prop_cardinality_min", val_cardinality=(2, None), parent=sec)
+assert prop_card_min.val_cardinality == (2, None)
+prop_card_min.val_cardinality
