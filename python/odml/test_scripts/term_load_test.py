c528c64447cdcfaf21ba70aaa44e42fbddbe9361
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-14 12:02:12 +0200

    [py/odml/test] Collect file info

--- python/odml/test_scripts/term_load_test.py
+++ python/odml/test_scripts/term_load_test.py
@@ -8,5 +8,12 @@ terminology.load(terminology.REPOSITORY)
 
 cache_dir = os.path.join(tempfile.gettempdir(), "odml.cache")
 
+orig_map = {}
 for fname in os.listdir(cache_dir):
-    print(fname)
+    spn = fname.split('.')
+    fn_mtime = os.path.getmtime(os.path.join(cache_dir, fname))
+    orig_map[spn[1]] = [spn[0], fn_mtime]
+
+print(orig_map)
+
+refresh_map = {}
