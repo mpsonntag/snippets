0f0e12c97b3e2f5301d6525505b795b9d806d060
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-14 12:13:05 +0200

    [py/odml/test/term] Add compare func

--- python/odml/test_scripts/term_load_test.py
+++ python/odml/test_scripts/term_load_test.py
@@ -8,12 +8,25 @@ terminology.load(terminology.REPOSITORY)
 
 cache_dir = os.path.join(tempfile.gettempdir(), "odml.cache")
 
-orig_map = {}
-for fname in os.listdir(cache_dir):
-    spn = fname.split('.')
-    fn_mtime = os.path.getmtime(os.path.join(cache_dir, fname))
-    orig_map[spn[1]] = [spn[0], fn_mtime]
 
-print(orig_map)
+def current_cache_files_map():
+    curr_map = {}
+    for fnm in os.listdir(cache_dir):
+        spn = fnm.split('.')
+        fn_mtime = os.path.getmtime(os.path.join(cache_dir, fnm))
+        curr_map[spn[1]] = [spn[0], fn_mtime]
 
-refresh_map = {}
+    return curr_map
+
+
+orig_map = current_cache_files_map()
+
+# test cache does not change
+terminology.load(terminology.REPOSITORY)
+
+load_map = current_cache_files_map()
+
+assert(len(orig_map) == len(load_map))
+for curr_file in orig_map:
+    assert curr_file in load_map
+    assert orig_map[curr_file] == load_map[curr_file]
