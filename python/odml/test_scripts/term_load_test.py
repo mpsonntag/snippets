e0921f33aac29a94aa2d60f8fd422c1b19848bc7
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-14 13:37:24 +0200

    [py/odml/test/term] Add tests

--- python/odml/test_scripts/term_load_test.py
+++ python/odml/test_scripts/term_load_test.py
@@ -19,14 +19,28 @@ def current_cache_files_map():
     return curr_map
 
 
+# Fetch current cache content
 orig_map = current_cache_files_map()
 
-# test cache does not change
+# Test cache content does not change
 terminology.load(terminology.REPOSITORY)
-
 load_map = current_cache_files_map()
 
 assert(len(orig_map) == len(load_map))
 for curr_file in orig_map:
     assert curr_file in load_map
     assert orig_map[curr_file] == load_map[curr_file]
+
+# Test refresh loads same cached files but changes them.
+# Different mtimes and id strings are sufficient.
+terminology.refresh(terminology.REPOSITORY)
+refresh_map = current_cache_files_map()
+assert(len(orig_map) == len(refresh_map))
+for curr_file in orig_map:
+    assert curr_file in refresh_map
+    # Check different id
+    is_equ = orig_map[curr_file][1] == refresh_map[curr_file][1]
+    print("%s, %s, %s, %s" % (curr_file, orig_map[curr_file][1], refresh_map[curr_file][1], is_equ))
+    assert orig_map[curr_file][0] == refresh_map[curr_file][0]
+    # Check different mtime
+    assert orig_map[curr_file][1] < refresh_map[curr_file][1]
