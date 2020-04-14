5683eb8e83366a607ba0ad8362dac09f585032b4
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-14 13:45:12 +0200

    [py/odml/test/term] Cleanup

--- python/odml/test_scripts/term_load_test.py
+++ python/odml/test_scripts/term_load_test.py
@@ -1,46 +1,59 @@
+"""
+Script testing odml cache refresh
+"""
+
 import os
 import tempfile
 
 from odml import terminology
 
 
-terminology.load(terminology.REPOSITORY)
-
-cache_dir = os.path.join(tempfile.gettempdir(), "odml.cache")
+CACHE_DIR = os.path.join(tempfile.gettempdir(), "odml.cache")
 
 
 def current_cache_files_map():
+    """
+    Returns a dict mapping the basefilenames of cached odml files
+    to their md5 hash and mtime.
+
+    :return: dict of the format {filename: [md5_hash, mtime]}
+    """
     curr_map = {}
-    for fnm in os.listdir(cache_dir):
+    for fnm in os.listdir(CACHE_DIR):
         spn = fnm.split('.')
-        fn_mtime = os.path.getmtime(os.path.join(cache_dir, fnm))
+        fn_mtime = os.path.getmtime(os.path.join(CACHE_DIR, fnm))
         curr_map[spn[1]] = [spn[0], fn_mtime]
 
     return curr_map
 
 
-# Fetch current cache content
-orig_map = current_cache_files_map()
+def main():
+    terminology.load(terminology.REPOSITORY)
 
-# Test cache content does not change
-terminology.load(terminology.REPOSITORY)
-load_map = current_cache_files_map()
+    # Fetch current cache content
+    orig_map = current_cache_files_map()
 
-assert(len(orig_map) == len(load_map))
-for curr_file in orig_map:
-    assert curr_file in load_map
-    assert orig_map[curr_file] == load_map[curr_file]
+    # Test cache content does not change
+    terminology.load(terminology.REPOSITORY)
+    load_map = current_cache_files_map()
 
-# Test refresh loads same cached files but changes them.
-# Different mtimes and id strings are sufficient.
-terminology.refresh(terminology.REPOSITORY)
-refresh_map = current_cache_files_map()
-assert(len(orig_map) == len(refresh_map))
-for curr_file in orig_map:
-    assert curr_file in refresh_map
-    # Check different id
-    is_equ = orig_map[curr_file][1] == refresh_map[curr_file][1]
-    print("%s, %s, %s, %s" % (curr_file, orig_map[curr_file][1], refresh_map[curr_file][1], is_equ))
-    assert orig_map[curr_file][0] == refresh_map[curr_file][0]
-    # Check different mtime
-    assert orig_map[curr_file][1] < refresh_map[curr_file][1]
+    assert len(orig_map) == len(load_map)
+    for curr_file in orig_map:
+        assert curr_file in load_map
+        assert orig_map[curr_file] == load_map[curr_file]
+
+    # Test refresh loads same cached files but changes them.
+    # Different mtimes and id strings are sufficient.
+    terminology.refresh(terminology.REPOSITORY)
+    refresh_map = current_cache_files_map()
+    assert len(orig_map) == len(refresh_map)
+    for curr_file in orig_map:
+        assert curr_file in refresh_map
+        # Check different id
+        assert orig_map[curr_file][0] == refresh_map[curr_file][0]
+        # Check different mtime
+        assert orig_map[curr_file][1] < refresh_map[curr_file][1]
+
+
+if __name__ == "__main__":
+    main()
