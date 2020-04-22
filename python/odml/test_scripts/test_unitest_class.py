254ad2f692d70c7550d3f7d917fa48405706129a
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-22 10:05:06 +0200

    [py/odml/test] Add unittest test class

---
+++ python/odml/test_scripts/test_unitest_class.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestUnitTestClass(unittest.TestCase):
+    def setUp(self):
+        print("set up")
+
+    def tearDown(self):
+        print("tear down")
+
+    def test_some_func(self):
+        print("test some func")
+
+    def test_some_method(self):
+        print("test some method")
