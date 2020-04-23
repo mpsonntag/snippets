d25ebbeb53376a467bc41735e08ce10d84792bc8
M. Sonntag <michael.p.sonntag@gmail.com>
2020-04-23 15:44:22 +0200

    [py/odml/test] Add deprecation warn tests

--- python/odml/test_scripts/test_unitest_class.py
+++ python/odml/test_scripts/test_unitest_class.py
@@ -1,6 +1,15 @@
 import unittest
+import warnings
 
 from os import path
+
+
+def formatted_warnings(msg, category=None, *args, **kwargs):
+    final_msg = "%s\n" % str(msg)
+    if category:
+        final_msg = "%s: %s" % (category.__name__, final_msg)
+
+    return final_msg
 
 
 class TestUnitTestClass(unittest.TestCase):
@@ -8,6 +17,8 @@ class TestUnitTestClass(unittest.TestCase):
         print("set up")
         core_name = path.basename(path.splitext(__file__)[0])
         print("File: %s" % core_name)
+        self.warnings = warnings
+        self.warnings.formatwarning = formatted_warnings
 
     def tearDown(self):
         print("tear down")
@@ -17,6 +28,11 @@ class TestUnitTestClass(unittest.TestCase):
 
     def test_some_method(self):
         print("test some method")
+
+    def test_deprecation_warning(self):
+        self.warnings.warn("Method deprecated", DeprecationWarning)
+        # warnings.simplefilter("ignore")
+        self.warnings.warn("Some other stuff", DeprecationWarning)
 
 
 if __name__ == "__main__":
