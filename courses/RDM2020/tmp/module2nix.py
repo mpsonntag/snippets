c0f030d6f49a5f27583dfed969b5674bb6290049
M. Sonntag <michael.p.sonntag@gmail.com>
2020-05-11 10:58:21 +0200

    [RDM2020] Cleanup module2nix file

--- courses/RDM2020/tmp/module2nix.py
+++ courses/RDM2020/tmp/module2nix.py
@@ -16,48 +16,53 @@ b108 = nixf.create_block("joe108", "nix.session")
 b147 = nixf.create_block("joe147", "nix.session")
 b151 = nixf.create_block("joe151", "nix.session")
 
-m_gen = nixf.create_section("General","odml.general")
+m_gen = nixf.create_section("General", "odml.general")
 m_gen.create_property("Experimenter", values_or_dtype='Alexa Riehle')
 m_gen.create_property("Institution", values_or_dtype='CNRS, Marseille, France')
 m_gen.create_property("RelatedPublications", values_or_dtype='doi: 10.1523/JNEUROSCI.5441-08.2009')
 
-m_exp = nixf.create_section("Experiment","odml.experiment")
+m_exp = nixf.create_section("Experiment", "odml.experiment")
 m_exp.create_property("Task", values_or_dtype="DelayedCenterOut")
 m_subj = m_exp.create_section("Subject", "odml.subject")
 m_subj.create_property("Name", values_or_dtype="Joe")
 m_subj.create_property("Species", values_or_dtype="Macaca mulatta")
 m_subj.create_property("Sex", values_or_dtype="male")
-m_rec = m_exp.create_section("Recording","odml.recording")
+m_rec = m_exp.create_section("Recording", "odml.recording")
 m_rec.create_property("BrainArea", values_or_dtype="M1")
 m_rec.create_property("RecordingType", values_or_dtype="extracellular")
 m_rec.create_property("SpikeSortingMethod", values_or_dtype="WindowDiscriminator")
 
 # trial conditions:
-condnames = {1 : "full", 2 : "2 of 6", 3 : "3 of 6"}
-m_cond = m_exp.create_section("TrialConditions","odml.conditions")
+condnames = {1: "full", 2: "2 of 6", 3: "3 of 6"}
+m_cond = m_exp.create_section("TrialConditions", "odml.conditions")
+
 
 def mkcond(cond, target):
-    condname = "condition %d target %d" % (cond,target)
+    condname = "condition %d target %d" % (cond, target)
     sec = m_cond.create_section(condname, "odml.section")
     sec.create_property("BehavioralCondition", values_or_dtype=cond)
     sec.create_property("BehavioralConditionName", values_or_dtype=condnames[cond])
     sec.create_property("Target", values_or_dtype=target)
     return sec
 
-m_infiles = {"joe097":"joe097-23457.gdf", "joe108":"joe108-124567.gdf", "joe147":"joe147-12467.gdf", "joe151":"joe151-12346.gdf"}
-#m_mlfiles = {"joe097":"joe097-5-C3-MO.mat", "joe108":"joe108-4-C3-MO.mat", "joe147":"", "joe151":""}
 
-m_conds = [[mkcond(c,t) for t in range(1,7) ] for c in range(1,4) ]
+m_infiles = {"joe097": "joe097-23457.gdf", "joe108": "joe108-124567.gdf",
+             "joe147": "joe147-12467.gdf", "joe151": "joe151-12346.gdf"}
+# m_mlfiles = {"joe097": "joe097-5-C3-MO.mat", "joe108": "joe108-4-C3-MO.mat",
+#              "joe147": "", "joe151": ""}
 
-m_sess = nixf.create_section("Sessions","odml.section")
-for sess in ["joe097", "joe108", "joe147", "joe151" ]:
-    m_s1 = m_sess.create_section(sess,"odml.session")
+m_conds = [[mkcond(c, t) for t in range(1, 7)] for c in range(1, 4)]
+
+m_sess = nixf.create_section("Sessions", "odml.section")
+for sess in ["joe097", "joe108", "joe147", "joe151"]:
+    m_s1 = m_sess.create_section(sess, "odml.session")
     m_s1id = m_s1.create_property("SessionID", values_or_dtype=sess)
     m_s1infile = m_s1.create_property("InputFile", values_or_dtype=m_infiles[sess])
-    m_s1subject = m_s1.create_section("Subject","odml.subject")
+    m_s1subject = m_s1.create_section("Subject", "odml.subject")
     m_s1subject.link = m_s1subject
-    m_s1conds = m_s1.create_section("TrialConditions","odml.conditions")
+    m_s1conds = m_s1.create_section("TrialConditions", "odml.conditions")
     m_s1conds.link = m_cond
+
 
 # -- Next block
 def mkunit(sblock, unitid):
@@ -75,17 +80,20 @@ def mkunit(sblock, unitid):
         # load motion end times
         medata = loadtxt('asciidata/%s-%d-C3-MO_MEevents_%02d.dat' % (sblock.name, unitid, target),
                          dtype=float)
-        medata = medata - 1000.  # motion end time is stored as array index, so subtract the time offset
+        # motion end time is stored as array index, so subtract the time offset
+        medata = medata - 1000.
         # load trial start times
         tsdata = loadtxt('asciidata/%s-%d-C3-TS_MOevents_%02d.dat' % (sblock.name, unitid, target),
                          dtype=float)
-        tsdata = -tsdata  # calculate trial start relative to motion from MO time in TS-aligned data
+        # calculate trial start relative to motion from MO time in TS-aligned data
+        tsdata = -tsdata
         # load spike times
         stf = open('asciidata/%s-%d-C3-MO_spiketrains_%02d.dat' % (sblock.name, unitid, target),
                    'r')
         stdata = []
         for line in stf:
-            st = [int(i) - 1000 for i in line.split()]  # shift by -1000 ms for alignment to MO
+            # shift by -1000 ms for alignment to MO
+            st = [int(i) - 1000 for i in line.split()]
             stdata.append(st)
         stf.close()
         # _for line
@@ -95,31 +103,42 @@ def mkunit(sblock, unitid):
             "SpikeActivity Unit %d Target %d" % (unitid, target), "nix.timeseries.binary",
             data=smxdata.T)
         spikeactivity.definition = "Array of spike occurrences aligned to movement onset (MO)"
-        sa_dim1 = spikeactivity.append_sampled_dimension(1.)  # 1 ms sampling interval
-        sa_dim1.offset = -1000.  # data aligned to MO
+        # 1 ms sampling interval
+        sa_dim1 = spikeactivity.append_sampled_dimension(1.)
+        # data aligned to MO
+        sa_dim1.offset = -1000.
         sa_dim1.unit = "ms"
         sa_dim1.label = "time"
-        sa_dim2 = spikeactivity.append_set_dimension()  # trials
+        # trials
+        sa_dim2 = spikeactivity.append_set_dimension()
         sa_dim1.label = "trial"
         spikeactivity.sources.append(su)
         # mov tag
         # this is not so great because the need to define the positions an extents as dataarrays,
         # so stick to the movement epochs as tags for each trial
-        # movtag = filter( lambda x : x.name == "Arm movement epochs for Target %d" % (target), sblock.multi_tags )
+        # movtag = filter(lambda x : x.name == "Arm movement epochs for Target %d" % (target),
+        #                 sblock.multi_tags )
         # if not movtag:
-        #    MOlst = sblock.create_data_array("MO times for Target %d" % (target), "nix.positions", data=[[0.0,tr] for tr in range(0,smxdata.shape[0])])
+        #    da_name = "MO times for Target %d" % (target)
+        #    da_data = [[0.0,tr] for tr in range(0,smxdata.shape[0])]
+        #    MOlst = sblock.create_data_array(da_name, "nix.positions", data=da_data)
         #    MOdim1 = MOlst.append_sampled_dimension(1.)
         #    MOdim1.unit = "ms"
         #    MOdim1.label = "time"
         #    MOdim2 = MOlst.append_set_dimension()
         #    MOdim2.label = "trial"
-        #    MElst = sblock.create_data_array("Movement durations for Target %d" % (target), "nix.positions", data=[[medata[tr],0] for tr in range(0,smxdata.shape[0])])
+        #
+        #    da_name = "Movement durations for Target %d" % (target)
+        #    da_data = [[medata[tr],0] for tr in range(0,smxdata.shape[0])]
+        #    MElst = sblock.create_data_array(da_name, "nix.positions", data=da_data)
         #    MEdim1 = MElst.append_sampled_dimension(1.)
         #    MEdim1.unit = "ms"
         #    MEdim1.label = "time"
         #    MEdim2 = MElst.append_set_dimension()
         #    MEdim2.label = "trial"
-        #    mov = sblock.create_multi_tag("Arm movement epochs for Target %d" % (target), "nix.epoch", MOlst)
+        #
+        #    mt_name = "Arm movement epochs for Target %d" % (target)
+        #    mov = sblock.create_multi_tag(mt_name, "nix.epoch", MOlst)
         #    mov.definition = "Epochs between detected movement onset (MO) and movement end (ME)"
         #    mov.extents = MElst
         #    mov.units = ["ms",]
@@ -130,7 +149,9 @@ def mkunit(sblock, unitid):
         # loop over all trials for this target
         for tr in range(0, smxdata.shape[0]):
             trialcnt += 1
-            # spikeactivity = sblock.create_data_array("SpikeActivity Unit %d Trial %03d" % (unitid,trialcnt),"nix.timeseries.binary",data=smxdata[tr])
+            # da_name = "SpikeActivity Unit %d Trial %03d" % (unitid,trialcnt)
+            # da_type = "nix.timeseries.binary"
+            # spikeactivity = sblock.create_data_array(da_name, da_type, data=smxdata[tr])
             # spikeactivity.definition = "Array of spike occurrences aligned to movement onset (MO)"
             # sa_dim.offset = -1000. # data aligned to MO
             # sa_dim.unit = "ms"
@@ -145,7 +166,8 @@ def mkunit(sblock, unitid):
             spiketimes.label = "spikes"
             # spike train as multitag
             # spikepos = [[x,tr] for x in stdata[tr]]
-            # spiketrain = sblock.create_multi_tag("Spiketrain Unit %d Trial %03d" % (unitid,trialcnt), "nix.spiketrain",spikepos)
+            # mt_name = "Spiketrain Unit %d Trial %03d" % (unitid,trialcnt)
+            # spiketrain = sblock.create_multi_tag(mt_name, "nix.spiketrain",spikepos)
             # spiketrain.definition = "Spike times aligned to movement onset (MO)"
             # spiketrain.references.append(spikeactivity)
             # assign sources
@@ -156,9 +178,9 @@ def mkunit(sblock, unitid):
                 target - 1]  # so far all data are 'C3' -> index 2 in conds
             spiketimes.metadata = m_conds[2][target - 1]
             # trial as tag
-            trialtag = filter(lambda x: x.name == "Trial %03d" % (trialcnt), sblock.tags)
+            trialtag = filter(lambda x: x.name == "Trial %03d" % trialcnt, sblock.tags)
             if not trialtag:
-                trial = sblock.create_tag("Trial %03d" % (trialcnt), "nix.trial", [tsdata[tr], tr])
+                trial = sblock.create_tag("Trial %03d" % trialcnt, "nix.trial", [tsdata[tr], tr])
                 trial.definition = "Trial start (TS) relative to motion onset (MO)"
                 trial.extent = [3000., 0]  # trial length of 3000ms is arbitrary
                 trial.units = ["ms"]
@@ -168,14 +190,14 @@ def mkunit(sblock, unitid):
 
             trial.references.append(spikeactivity)
             # arm movement period as tag
-            movtag = filter(lambda x: x.name == "Arm movement epoch Trial %03d" % (trialcnt),
+            movtag = filter(lambda x: x.name == "Arm movement epoch Trial %03d" % trialcnt,
                             sblock.tags)
             if not movtag:
-                mov = sblock.create_tag("Arm movement epoch Trial %03d" % (trialcnt), "nix.epoch",
+                mov = sblock.create_tag("Arm movement epoch Trial %03d" % trialcnt, "nix.epoch",
                                         [0.0, tr])
                 mov.definition = "Epoch between detected movement onset (MO) and movement end (ME)"
-                mov.extent = [medata[tr],
-                              0]  # because motion onset is at 0.0, duration is equal to end time
+                # because motion onset is at 0.0, duration is equal to end time
+                mov.extent = [medata[tr], 0]
                 mov.units = ["ms", ]
             else:
                 mov = movtag[0]
@@ -185,15 +207,14 @@ def mkunit(sblock, unitid):
 # _def mkunit
 
 # -- next block
+# mkunit(b097,5)
+# mkunit(b108,4)
+# mkunit(b108,7)
+# mkunit(b147,1)
+# mkunit(b151,1)
 
-#mkunit(b097,5)
-#mkunit(b108,4)
-#mkunit(b108,7)
-#mkunit(b147,1)
-#mkunit(b151,1)
 
 # -- next block
-
 nixf.close()
 
 # -- next block
@@ -212,23 +233,24 @@ for b in nixf.blocks:
 b108 = nixf.blocks["joe108"]
 print(b108.sources)
 
-dalst = filter( lambda x :
+dalst = filter(lambda x:
                ("SpikeActivity" in x.name) &
-               (filter( lambda s : s.name == "Unit 7", x.sources) != []) &
+               (filter(lambda sec: sec.name == "Unit 7", x.sources) != []) &
                (x.metadata['Target'] == 2) &
                (x.metadata['BehavioralCondition'] == 3),
                b108.data_arrays)
 
 print(dalst)
-len(dalst)
-dalst[0].shape
-spikedata = dalst[0]
-spikedata.shape
-yyy = nonzero(spikedata)
-[tind,jind] = nonzero(spikedata)
-scatter(tind,jind)
+print("len: %d; shape: %s" % (len(dalst), dalst[0].shape))
 
-[jbla,tbla] = nonzero(array(spikedata).T)
-scatter(tbla,jbla)
+spikedata = dalst[0]
+print(spikedata.shape)
+
+yyy = nonzero(spikedata)
+[tind, jind] = nonzero(spikedata)
+scatter(tind, jind)
+
+[jbla, tbla] = nonzero(array(spikedata).T)
+scatter(tbla, jbla)
 
 nixf.close()
