56c2ef3998a3ba4ca3502dfff91ff0fd60eff2ad
M. Sonntag <michael.p.sonntag@gmail.com>
2020-05-27 18:23:24 +0200

    [py/odml] Add INM6 root hand validation

--- python/odml/scripts/INM6_custom_validations.py
+++ python/odml/scripts/INM6_custom_validations.py
@@ -45,3 +45,54 @@ def verify_hand_hemisphere_prop(obj):
         if invalid:
             msg = "Invalid active hand/hemisphere pair"
             yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
+
+
+def verify_hand_hemisphere_root(obj):
+    validation_id = IssueID.custom_validation
+
+    try:
+        # path hemisphere: root/S[Subject]/S[ArrayImplant]/P[Hemisphere]
+        hemisphere = obj.sections["Subject"].sections["ArrayImplant"].properties["Hemisphere"]
+        # path active hand: root/S[Subject]/P[ActiveHand]
+        active_hand = obj.sections["Subject"].properties["ActiveHand"]
+    except KeyError:
+        msg = "Invalid location of active hand or hemisphere property"
+        yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
+        return
+
+    invalid = False
+    if not hemisphere or not hemisphere.values:
+        invalid = True
+    elif not active_hand or not active_hand.values:
+        invalid = True
+    elif not hemisphere.values[0] in ["left", "right"] or \
+            not active_hand.values[0] in ["left", "right"]:
+        invalid = True
+    elif hemisphere.values[0] == "right" and not active_hand.values[0] == "left":
+        invalid = True
+    elif hemisphere.values[0] == "left" and not active_hand.values[0] == "right":
+        invalid = True
+
+    if invalid:
+        msg = "Invalid active hand/hemisphere pair"
+        yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
+
+
+# Create empty custom validation and add custom handler functions
+Validation.register_handler("property", check_left_right)
+
+# Edited versions of the original file i140703-001.odml
+filename = "invalid_hand_hemisphere_entry_example_i140703-001.odml"
+# filename = "invalid_hand_hemisphere_value_example_i140703-001.odml"
+invalid_doc = odml.load(filename)
+custom_validation = Validation(invalid_doc, validate=False, reset=True)
+custom_validation.register_custom_handler("property", check_left_right)
+custom_validation.register_custom_handler("property", verify_hand_hemisphere_prop)
+custom_validation.register_custom_handler("odML", verify_hand_hemisphere_root)
+
+# Run validation and print report
+custom_validation.report()
+
+# Print individual warnings and errors
+for err in custom_validation.errors:
+    print(err)
