83f61280e1ea3aef3a1de442a73fbaad175a95cb
M. Sonntag <michael.p.sonntag@gmail.com>
2020-05-27 18:22:25 +0200

    [py/odml] Add INM6 prop hand validation

--- python/odml/scripts/INM6_custom_validations.py
+++ python/odml/scripts/INM6_custom_validations.py
@@ -11,3 +11,37 @@ def check_left_right(obj):
 
         msg = "Invalid '%s' value: %s" % (obj.name, obj.values)
         yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
+
+
+def verify_hand_hemisphere_prop(obj):
+    validation_id = IssueID.custom_validation
+
+    # path hemisphere: root/S[Subject]/S[ArrayImplant]/P[Hemisphere]
+    hemisphere = "Hemisphere"
+    # path active hand: root/S[Subject]/P[ActiveHand]
+    active_hand = "ActiveHand"
+    if obj.name in [hemisphere, active_hand]:
+        other = None
+        try:
+            if obj.name == hemisphere:
+                other = obj.parent.parent.properties[active_hand]
+            elif obj.name == active_hand:
+                other = obj.parent.sections["ArrayImplant"].properties[hemisphere]
+        except KeyError:
+            msg = "Invalid location of active hand/hemisphere property"
+            yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
+            return
+
+        invalid = False
+        if not other or not obj.values or not other.values:
+            invalid = True
+        elif not obj.values[0] in ["left", "right"] or not other.values[0] in ["left", "right"]:
+            invalid = True
+        elif obj.values[0] == "right" and not other.values[0] == "left":
+            invalid = True
+        elif obj.values[0] == "left" and not other.values[0] == "right":
+            invalid = True
+
+        if invalid:
+            msg = "Invalid active hand/hemisphere pair"
+            yield ValidationError(obj, msg, LABEL_WARNING, validation_id)
