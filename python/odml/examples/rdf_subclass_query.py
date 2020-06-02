a5bbad6d8c4b348eb6b3b9a76cd05263a476afc3
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-02 19:10:45 +0200

    [py/odml/rdf] Add owlrl example

--- python/odml/examples/rdf_subclass_query.py
+++ python/odml/examples/rdf_subclass_query.py
@@ -3,6 +3,8 @@ from odml.tools.rdf_converter import ODML_NS, RDFWriter
 from rdflib import Graph, Namespace, RDF, RDFS
 from rdflib.plugins.sparql import prepareQuery
 
+from owlrl import DeductiveClosure, RDFS_Semantics
+
 # Load a file containing subclasses to a default rdflib graph
 fname = "./rdf_subclass_query.rdf.xml"
 
@@ -22,3 +24,22 @@ for row in graph.query(curr_query):
     print(row.s)
 
 print(g.serialize(format='turtle').decode("utf-8"))
+
+
+# Test with an inference engine on top of the default rdflib graph
+graph_expand = Graph()
+graph_expand.parse(fname)
+
+DeductiveClosure(RDFS_Semantics).expand(graph_expand)
+
+q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
+curr_query = prepareQuery(q_string, initNs={"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS})
+
+for row in graph_expand.query(curr_query):
+    print(row.s)
+
+q_string = "SELECT * WHERE {?s rdf:type odml:Section .}"
+curr_query = prepareQuery(q_string, initNs={"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS})
+
+for row in graph_expand.query(curr_query):
+    print(row.s)
