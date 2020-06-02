4a91a525b792856f8c63587e2c50c749fafd58c1
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-02 19:23:43 +0200

    [py/odml/rdf] Move redundant code to func

--- python/odml/examples/rdf_subclass_query.py
+++ python/odml/examples/rdf_subclass_query.py
@@ -5,48 +5,37 @@ from rdflib.plugins.sparql import prepareQuery
 
 from owlrl import DeductiveClosure, RDFS_Semantics
 
+
+def run_query(curr_graph):
+    q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
+    curr_query = prepareQuery(q_string, initNs=NAMESPACE_MAP)
+
+    for row in curr_graph.query(curr_query):
+        print(row.s)
+
+    q_string = "SELECT * WHERE {?s rdf:type odml:Section .}"
+    curr_query = prepareQuery(q_string, initNs=NAMESPACE_MAP)
+
+    for row in curr_graph.query(curr_query):
+        print(row.s)
+
+    print(curr_graph.serialize(format='turtle').decode("utf-8"))
+
+
+FNAME = "./rdf_subclass_query.rdf.xml"
+NAMESPACE_MAP = {"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS}
+
 # Load a file containing subclasses to a default rdflib graph
-fname = "./rdf_subclass_query.rdf.xml"
-namespace_map = {"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS}
-
-
 graph = Graph()
-graph.parse(fname)
-
-q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
-curr_query = prepareQuery(q_string, initNs=namespace_map)
-
-for row in graph.query(curr_query):
-    print(row.s)
-
-q_string = "SELECT * WHERE {?s rdf:type odml:Section .}"
-curr_query = prepareQuery(q_string, initNs=namespace_map)
-
-for row in graph.query(curr_query):
-    print(row.s)
-
-print(graph.serialize(format='turtle').decode("utf-8"))
-
+graph.parse(FNAME)
+run_query(graph)
 
 # Test with an inference engine on top of the default rdflib graph
 graph_expand = Graph()
-graph_expand.parse(fname)
+graph_expand.parse(FNAME)
 
 DeductiveClosure(RDFS_Semantics).expand(graph_expand)
-
-q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
-curr_query = prepareQuery(q_string, initNs=namespace_map)
-
-for row in graph_expand.query(curr_query):
-    print(row.s)
-
-q_string = "SELECT * WHERE {?s rdf:type odml:Section .}"
-curr_query = prepareQuery(q_string, initNs=namespace_map)
-
-for row in graph_expand.query(curr_query):
-    print(row.s)
-
-print(graph_expand.serialize(format='turtle').decode("utf-8"))
+run_query(graph_expand)
 
 # For future test cases if a proper web endpoint is available the following implementation might
 # be a faster RDFS inference implementation e.g. for subclass inference
