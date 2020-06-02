b10607c6d20b1afed0d2a72d897b2a0ad228d0b1
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-02 19:24:02 +0200

    [py/odml/rdf] Add docstrings

--- python/odml/examples/rdf_subclass_query.py
+++ python/odml/examples/rdf_subclass_query.py
@@ -1,3 +1,10 @@
+"""
+This script tests how to load an RDF file to an rdflib graph
+and query the content.
+Meant to test various additional libraries for their RDF inference/reasoning
+since the basic rdflib implementation does not supply inference.
+"""
+
 from odml.tools.rdf_converter import ODML_NS
 
 from rdflib import Graph, Namespace, RDF, RDFS
@@ -6,7 +13,20 @@ from rdflib.plugins.sparql import prepareQuery
 from owlrl import DeductiveClosure, RDFS_Semantics
 
 
+FNAME = "./rdf_subclass_query.rdf.xml"
+NAMESPACE_MAP = {"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS}
+
+
 def run_query(curr_graph):
+    """
+    Runs two queries on a provided graph. The first query tests
+    a subclass "Cell" directly, the second query tests whether this
+    subclass can also be found via its superclass "Section".
+
+    Prints all results and the full graph to the command line.
+
+    :param curr_graph: an rdflib in memory graph
+    """
     q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
     curr_query = prepareQuery(q_string, initNs=NAMESPACE_MAP)
 
@@ -20,10 +40,6 @@ def run_query(curr_graph):
         print(row.s)
 
     print(curr_graph.serialize(format='turtle').decode("utf-8"))
-
-
-FNAME = "./rdf_subclass_query.rdf.xml"
-NAMESPACE_MAP = {"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS}
 
 # Load a file containing subclasses to a default rdflib graph
 graph = Graph()
