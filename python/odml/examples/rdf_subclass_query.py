9f48085236a2a2b446390e7c1fd52bf886ba0cdc
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-02 19:09:57 +0200

    [py/odml/rdf] Use normal graph query

--- python/odml/examples/rdf_subclass_query.py
+++ python/odml/examples/rdf_subclass_query.py
@@ -1,31 +1,24 @@
-import odml
-
 from odml.tools.rdf_converter import ODML_NS, RDFWriter
 
 from rdflib import Graph, Namespace, RDF, RDFS
 from rdflib.plugins.sparql import prepareQuery
 
-fname = "/home/sommer/Chaos/tmp/odml/rdf/sub_class_query.rdf"
-
-doc = odml.Document()
-sec = odml.Section(name="Cell", type="cell", parent=doc)
-
-RDFWriter(doc).write_file("%s.ttl" % fname)
-odml.save(doc, fname, "RDF")
+# Load a file containing subclasses to a default rdflib graph
+fname = "./rdf_subclass_query.rdf.xml"
 
 graph = Graph()
 graph.parse(fname)
 
 q_string = "SELECT * WHERE {?s rdf:type odml:Cell .}"
-
 curr_query = prepareQuery(q_string, initNs={"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS})
 
 for row in graph.query(curr_query):
     print(row.s)
 
 q_string = "SELECT * WHERE {?s rdf:type odml:Section .}"
-
 curr_query = prepareQuery(q_string, initNs={"odml": Namespace(ODML_NS), "rdf": RDF, "rdfs": RDFS})
 
 for row in graph.query(curr_query):
     print(row.s)
+
+print(g.serialize(format='turtle').decode("utf-8"))
