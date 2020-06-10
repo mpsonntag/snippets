1a4441efdc33adc0e625c95e339702d90f00c17f
M. Sonntag <michael.p.sonntag@gmail.com>
2020-06-10 20:38:13 +0200

    [py/odml/rdf_crcns] Add full query examples

--- python/odml/scripts/rdf_crcns_query.py
+++ python/odml/scripts/rdf_crcns_query.py
@@ -77,3 +77,95 @@ SELECT * WHERE {
 for row in curr_graph.query(q):
     print("Doc: %s, Sec: %s, DataSecName: %s, Prop: %s" %
           (row.d, row.doisec, row.dataSecName, row.uri))
+
+# Searching for datasets that deal with Behavior or Extracellular recordings
+q = prepareQuery("""
+SELECT * WHERE {
+    ?d a odml:Document .
+    ?d odml:hasSection* ?s .
+    ?s odml:hasType ?sectype . 
+    ?s odml:hasName ?secname .
+    { SELECT ?doiSecName ?uri WHERE {
+        ?d a odml:Document .
+        ?d odml:hasSection* ?doisec .
+        ?doisec a odml:Identifier .
+        ?doisec odml:hasName ?doiSecName .
+        ?doisec odml:hasProperty ?uriprop .
+        ?uriprop odml:hasName "identifier" .
+        ?uriprop odml:hasValue ?urival .
+        ?urival ?uripred ?uri .
+        ?doisec odml:hasProperty ?tprop .
+        ?tprop odml:hasName "identifierType" .
+        ?tprop odml:hasValue ?tpropval .
+        ?tval ?tpred "DOI" .
+        FILTER (strstarts(str(?uripred), str(rdf:_)))
+    }}
+    FILTER (?sectype in ("Extracellular recordings", "Behavior"))
+}""", initNs=NAMESPACE_MAP)
+
+for row in curr_graph.query(q):
+    print("Doc: %s, SecName: %s, DoiName: %s, DOI: %s" %
+          (row.d, row.secname, row.doiSecName, row.uri))
+
+# Searching for datasets that contain specific keywords
+q = prepareQuery("""
+SELECT * WHERE {
+    ?d a odml:Document .
+    ?d odml:hasSection* ?s .
+    ?s odml:hasName ?secname .
+    ?s odml:hasProperty ?p .
+    ?p odml:hasName "task_keyword" .
+    ?p odml:hasValue ?pval .
+    ?pval ?valpred ?val .
+    { SELECT ?doiSecName ?uri WHERE {
+        ?d a odml:Document .
+        ?d odml:hasSection* ?doisec .
+        ?doisec a odml:Identifier .
+        ?doisec odml:hasName ?doiSecName .
+        ?doisec odml:hasProperty ?uriprop .
+        ?uriprop odml:hasName "identifier" .
+        ?uriprop odml:hasValue ?urival .
+        ?urival ?uripred ?uri .
+        ?doisec odml:hasProperty ?tprop .
+        ?tprop odml:hasName "identifierType" .
+        ?tprop odml:hasValue ?tpropval .
+        ?tval ?tpred "DOI" .
+        FILTER (strstarts(str(?uripred), str(rdf:_)))
+    }}
+    FILTER (?val in ("reward", "response"))
+    FILTER (strstarts(str(?valpred), str(rdf:_)))
+}""", initNs=NAMESPACE_MAP)
+
+for row in curr_graph.query(q):
+    print("Doc: %s, SecName: %s, Value: %s, DOI: %s" %
+          (row.d, row.secname, row.val, row.uri))
+
+# Searching for datasets that contain diverse sets of values e.g. Organisation, Organism, etc
+q = prepareQuery("""
+SELECT * WHERE {
+    ?d a odml:Document .
+    ?d odml:hasSection* ?s .
+    ?s odml:hasName ?secname .
+    ?s odml:hasProperty ?p .
+    ?p odml:hasName ?propName .
+    ?p odml:hasValue ?pval .
+    ?pval ?valpred ?val .
+    { SELECT ?uri ?doiSecName WHERE {
+        ?d a odml:Document .
+        ?d odml:hasSection* ?doisec .
+        ?doisec a odml:Identifier .
+        ?doisec odml:hasName ?doiSecName .
+        ?doisec odml:hasProperty ?doiprop .
+        ?doiprop odml:hasName ?nameValue .
+        ?doiprop odml:hasValue ?urival .
+        ?urival ?uripred ?uri .
+        FILTER (?nameValue in ("identifier"))
+        FILTER (strstarts(str(?uripred), str(rdf:_)))
+    }}
+    FILTER (?val in ("Dataset/Neurophysiology", "CRCNS.org", "mice"))
+    FILTER (strstarts(str(?valpred), str(rdf:_)))
+}""", initNs=NAMESPACE_MAP)
+
+for row in curr_graph.query(q):
+    print("Doc: %s, SecName: %s, PropName: %s, Value: %s, DOI: %s" %
+          (row.d, row.secname, row.propName, row.val, row.uri))
