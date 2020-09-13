c127ba9d45a5f365f45a77f3617b916057a0bcb0
M. Sonntag <michael.p.sonntag@gmail.com>
2020-09-13 23:00:27 +0200

    [cmd/uptofile] Minimal server

--- cmd/uptofile/main.go
+++ cmd/uptofile/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
+func rootFunc(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "It's alive!")
+}
+
 func main() {
-	fmt.Println("I am here")
+	http.HandleFunc("/", rootFunc)
+	server := http.Server{
+		Addr: ":3030",
+	}
+
+	fmt.Fprintln(os.Stdout, "[Start] Listen and serve")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\n[Error] Server startup: '%v', abort...\n\n", err)
+		os.Exit(-1)
+	}
 }
