33237f05b32e7515bcaee2d1563e6ea4b1582417
M. Sonntag <michael.p.sonntag@gmail.com>
2020-09-14 11:46:34 +0200

    [cmd/uptofile] Add out file handling

--- cmd/uptofile/main.go
+++ cmd/uptofile/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var port = ":3030"
+var outdir = filepath.Join(os.Getenv("HOME"), "Chaos", "DL")
+var outfilepath = filepath.Join(outdir, "outfile.txt")
 
 func rootFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "It's alive!")
@@ -23,6 +26,22 @@ func processUploadFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if _, err := os.Stat(outdir); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "\n[Error] Output directory not found: '%v', abort...\n\n", outdir)
+		os.Exit(-1)
+	}
+
+	if _, err := os.Stat(outfilepath); os.IsNotExist(err) {
+		fmt.Fprint(os.Stdout, "\n[Start] Output file does not exist, creating.\n\n")
+		outfile, err := os.Create(outfilepath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "\n[Error] Could not create output file: '%v', abort ...\n\n", err)
+			os.Exit(-1)
+		}
+		outfile.Close()
+		fmt.Fprintf(os.Stdout, "\n[Start] Output file '%v' created.\n\n", outfilepath)
+	}
+
 	http.HandleFunc("/", rootFunc)
 	http.HandleFunc("/upload", uploadFormFunc)
 	http.HandleFunc("/uploaded", processUploadFunc)
