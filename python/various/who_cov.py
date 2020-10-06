c1df6ccf6dab7cd7f2b76815f5d839b011d35184
M. Sonntag <michael.p.sonntag@gmail.com>
2020-10-06 12:43:53 +0200

    [py/who_cov] Add country list

--- python/various/who_cov.py
+++ python/various/who_cov.py
@@ -2,6 +2,18 @@ import json
 import os
 import requests
 
+euro = {"at": "Austria", "be": "Belgium", "ba": "Bosnia and Herzegovina", "bg": "Bulgaria",
+        "hr": "Croatia", "cy": "Cyprus", "cz": "Czechia", "dk": "Denmark", "ee": "Estonia",
+        "fi": "Finland", "fr": "France", "de": "Germany", "gr": "Greece", "hu": "Hungary",
+        "ie": "Ireland", "it": "Italy", "lv": "Latvia", "li": "Liechtenstein",
+        "lt": "Lithuania", "lu": "Luxembourg", "nl": "Netherlands", "no": "Norway",
+        "po": "Poland", "pt": "Portugal", "ro": "Romania", "rs": "Serbia", "sk": "Slovakia",
+        "si": "Slovenia", "es": "Spain", "se": "Sweden", "ch": "Switzerland",
+        "gb": "United Kingdom"}
+
+regions = {"amro": ["us"],
+           "euro": euro.keys()}
+
 w_dir = os.path.join(os.environ.get("HOME"), "Chaos", "DL")
 
 furl = "https://covid19.who.int/page-data/region/euro/country/at/page-data.json"
