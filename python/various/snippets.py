57d354107bcae797e9b61877d7cc1e43e6dff43d
M. Sonntag <michael.p.sonntag@gmail.com>
2020-10-23 11:53:37 +0200

    [py/snippets] Add kwargs example

--- python/various/snippets.py
+++ python/various/snippets.py
@@ -1,3 +1,21 @@
 import hashlib
 
 print(hashlib.sha1(b"abcdefg@cd").hexdigest())
+
+
+def conditional_print(filename, local_style=True, custom_template=None):
+
+    print("filename: %s, local_style: %s, custom_template: %s" % (filename, local_style,
+                                                                  custom_template))
+
+
+def handle_kwargs(filename, **kwargs):
+
+    local_style = False
+    custom_template = None
+
+    if "local_style" in kwargs and isinstance(kwargs["local_style"], bool):
+        local_style = kwargs["local_style"]
+    if "custom_template" in kwargs and isinstance(kwargs["custom_template"], str):
+        custom_template = kwargs["custom_template"]
+    conditional_print(filename, local_style, custom_template)
