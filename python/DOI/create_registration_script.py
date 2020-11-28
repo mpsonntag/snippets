0720ef51af4baa21cb2886f2a2fc65b5ab82abcf
M. Sonntag <michael.p.sonntag@gmail.com>
2020-11-28 15:51:29 +0100

    [py/doi] Document required fields

--- python/DOI/create_registration_script.py
+++ python/DOI/create_registration_script.py
@@ -1,13 +1,24 @@
+# Automated registration id from "10.12751/g-node.[id]"
 REG_ID = ""
+# Repository owner
 REPO_OWN = ""
+# Repository name
 REPO = ""
+# Format YYYY-MM-DD
 REG_DATE = ""
 
+# DOI requestee email address
 EMAIL = ""
+# DOI requestee full name
 USER_FULL_NAME = ""
+# DOI request title
 TITLE = ""
+# Author citation list; ideally analogous to the DOI landing page citation
 CITATION = ""
 
-SERVER_USER = ""
+# Full ssh access name of the server hosting the DOI server instance
 DOI_SERVER = ""
+# User working on the DOI server
+SERVER_USER = ""
+# Full name of the person handling the registration
 ADMIN_NAME = ""
