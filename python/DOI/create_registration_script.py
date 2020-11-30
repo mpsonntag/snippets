34d6398c7dbdb13ccb27e25b7a0606f8350a90e3
M. Sonntag <michael.p.sonntag@gmail.com>
2020-11-30 14:27:02 +0100

    [py/doi] Add full pre doi func

--- python/DOI/create_registration_script.py
+++ python/DOI/create_registration_script.py
@@ -26,11 +26,11 @@ SERVER_USER = "__SERVER_USER__"
 ADMIN_NAME = "__FIRST LAST__"
 
 
-def print_part_one(f):
+def print_part_pre_doi(fp):
     """
     Print pre-registration block to file
 
-    :param f: filepointer
+    :param fp: filepointer
     """
     text_block = """
 # Part 1 - pre registration
@@ -45,7 +45,7 @@ def print_part_one(f):
 
     - Request Date from DOI XML: %s
     """ % (REPO_OWN, REPO, USER_FULL_NAME, EMAIL, DOI_SERVER, REG_ID, REG_ID, REG_DATE)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 
@@ -53,7 +53,7 @@ def print_part_one(f):
     - cd /gindata
     - ./annexcheck /gindata/gin-repositories/%s
     """ % REPO_OWN.lower()
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] check the datacite content at https://gin.g-node.org/%s/%s
@@ -61,7 +61,7 @@ def print_part_one(f):
     - title is useful and has no typos
     - license title, license content and license link match
     """ % (REPO_OWN, REPO)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] check DOI directory content
@@ -73,7 +73,7 @@ def print_part_one(f):
     -[ ] check zip download and suggested size
 -[ ] on gin.g-node.org, log in with "doi" user and fork https://gin.g-node.org/%s/%s
     """ % (REG_ID, REG_ID, REPO_OWN, REPO)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] fetch content and upload to DOI repo
@@ -82,7 +82,7 @@ def print_part_one(f):
     - sudo su root
     - ./syncannex %s/%s > %s_%s.log
     """ % (REPO_OWN.lower(), REPO_OWN, REPO, REPO_OWN.lower(), REPO)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] tag release on the DOI repository; run all commands using `gin git ...` 
@@ -93,14 +93,14 @@ def print_part_one(f):
     -[ ] sudo gin git tag 10.12751/g-node.%s
     -[ ] sudo gin git push --tags origin
     """ % (REPO.lower(), REG_ID)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] cleanup directory once tagging is done
     -[ ] sudo rm /data/doiprep/%s -r
     -[ ] sudo mv /data/doiprep/%s*.log /home/%s/logs/
     """ % (REPO.lower(), REPO_OWN, SERVER_USER)
-    f.write(text_block)
+    fp.write(text_block)
 
     text_block = """
 -[ ] email to TWachtler
@@ -112,12 +112,105 @@ the repository should be prepared for the DOI registration.
 Best,
 %s
     """ % (ADMIN_NAME.split()[0])
-    f.write(text_block)
+    fp.write(text_block)
+
+
+def print_part_pre_doi_full(fp):
+    text_block = """
+## Full DOI
+-[ ] check that the annex content is fully available on the gin server (srv5)
+    - cd /gindata
+    - ./annexcheck /gindata/gin-repositories/%s
+-[ ] check the datacite content at https://gin.g-node.org/%s/%s
+    - title is useful and has no typos
+    - license title, license content and license link match
+-[ ] log onto https://gin.g-node.org using the "doi" user (check G-Node vault for pw)
+    """ % (REPO_OWN, REPO_OWN, REPO)
+    fp.write(text_block)
+
+    text_block = """
+-[ ] manually fork https://gin.g-node.org/%s/%s to DOI user
+-[ ] log on to the doi server (srv6) and move to /data/doiprep
+-[ ] fetch content and upload annex data to the DOI repo on gin
+    - screen -S %s
+    - sudo su root
+    - ./syncannex %s/%s > %s-%s.log
+    """ % (REPO_OWN, REPO, REPO_OWN.lower(), REPO_OWN, REPO, REPO_OWN.lower(), REPO)
+    fp.write(text_block)
+
+    text_block = """
+-[ ] create DOI zip file
+    - screen -S %s
+    - sudo su root
+    - sudo ./makezip %s
+-[ ] sudo mv %s.zip /data/doi/10.12751/g-node.%s/10.12751_g-node.%s.zip
+    """ % (REPO_OWN.lower(), REPO.lower(), REPO.lower(), REG_ID, REG_ID)
+    fp.write(text_block)
+
+    text_block = """
+-[ ] create a tag release on the DOI repository; run all commands using `gin git ...` 
+     to avoid issues with local git annex or differently logged in gin/git users.
+    -[ ] cd /data/doiprep/%s
+    -[ ] sudo gin git status
+    -[ ] sudo gin git remote -v
+    -[ ] sudo gin git tag 10.12751/g-node.%s
+    -[ ] sudo gin git push --tags origin
+-[ ] clean up directory once tagging is done
+    -[ ] sudo rm /data/doiprep/%s -r
+    -[ ] sudo mv /data/doiprep/%s*.log /home/%s/logs/
+    """ % (REPO.lower(), REG_ID, REPO.lower(), REPO_OWN, SERVER_USER)
+    fp.write(text_block)
+
+    text_block = """
+-[ ] edit /data/doi/10.12751/g-node.%s/doi.xml file 
+     to include the actual size, the proper title and the proper license
+- move to local, create index.html from published doi.xml
+-[ ] Create the DOI landing page in the local staging directory and move it to the doi server
+    gindoid make-html https://doi.gin.g-node.org/10.12751/g-node.%s/doi.xml
+    scp index.html %s@%s:/home/%s/staging
+    sudo mv index.html /data/doi/10.12751/g-node.%s/index.html
+    sudo chown root:root /data/doi
+    sudo chmod ugo+rX -R /data/doi
+    """ % (REG_ID, REG_ID, SERVER_USER, DOI_SERVER, SERVER_USER, REG_ID)
+    fp.write(text_block)
+
+    text_block = """
+- https://doi.gin.g-node.org/10.12751/g-node.%s
+    -[ ] check page access, size, title, license name
+    -[ ] check all links
+    -[ ] check zip download and suggested size
+    """ % REG_ID
+    fp.write(text_block)
+
+    text_block = """
+-[ ] email TWachtler about the prepared DOI requests; use and forward the first registration request email
+
+To: gin@g-node.org
+
+Hi Thomas,
+
+the repository should be prepared for the DOI registration.
+
+In the doi.xml the following changes were made and the index.html page has been updated accordingly:
+- Title was updated.
+- License name and link was updated.
+- Zip size was added.
+
+Best,
+Michael
+    """
+    fp.write(text_block)
+
+    text_block = """
+    
+    """
+    fp.write(text_block)
 
 
 OUT_FILE = "%s_%s.md" % (REG_ID.lower(), REPO_OWN.lower())
 print("-- Writing to file %s" % OUT_FILE)
 with open(OUT_FILE, "w") as f:
-    print_part_one(f)
+    print_part_pre_doi(f)
+    print_part_pre_doi_full(f)
 
 print("-- Finished writing file %s" % OUT_FILE)
