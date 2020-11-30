bb2e50e586e9292671e4135c130a4d29d77e7b49
M. Sonntag <michael.p.sonntag@gmail.com>
2020-11-30 16:41:05 +0100

    [py/doi] Change block text line endings

--- python/DOI/create_registration_script.py
+++ python/DOI/create_registration_script.py
@@ -50,24 +50,22 @@ def print_part_pre_doi(fip):
     - DOI XML: %s:/data/doi/10.12751/g-node.%s/doi.xml
     - DOI target URL: https://doi.gin.g-node.org/10.12751/g-node.%s
 
-    - Request Date from DOI XML: %s
-    """ % (REPO_OWN, REPO, USER_FULL_NAME, EMAIL, DOI_SERVER, REG_ID, REG_ID, REG_DATE)
+    - Request Date from DOI XML: %s""" % (REPO_OWN, REPO, USER_FULL_NAME, EMAIL,
+                                          DOI_SERVER, REG_ID, REG_ID, REG_DATE)
     fip.write(text_block)
 
     text_block = """
 
 -[ ] GIN server (%s) check annex content
     - cd /gindata
-    - ./annexcheck /gindata/gin-repositories/%s
-    """ % (GIN_SERVER, REPO_OWN.lower())
+    - ./annexcheck /gindata/gin-repositories/%s""" % (GIN_SERVER, REPO_OWN.lower())
     fip.write(text_block)
 
     text_block = """
 - check the datacite content at https://gin.g-node.org/%s/%s
     -[ ] check if the repo is eligible to be published on gin
     -[ ] title is useful and has no typos
-    -[ ] license title, license content and license link match
-    """ % (REPO_OWN, REPO)
+    -[ ] license title, license content and license link match""" % (REPO_OWN, REPO)
     fip.write(text_block)
 
     text_block = """
@@ -80,8 +78,8 @@ def print_part_pre_doi(fip):
     -[ ] check title, license name
     -[ ] check all links
     -[ ] check zip download and compare size on server with size in `doi.xml`
--[ ] on gin.g-node.org, log in with "doi" user and fork https://gin.g-node.org/%s/%s
-    """ % (DOI_SERVER, REG_ID, REG_ID, REPO_OWN, REPO)
+-[ ] on gin.g-node.org, log in with "doi" user and fork https://gin.g-node.org/%s/%s""" % \
+                 (DOI_SERVER, REG_ID, REG_ID, REPO_OWN, REPO)
     fip.write(text_block)
 
     text_block = """
@@ -91,8 +89,8 @@ def print_part_pre_doi(fip):
     - cd %s/
     - screen -S %s
     - sudo su root
-    - ./syncannex %s/%s > %s_%s.log
-    """ % (DIR_DOI_PREP, REPO_OWN.lower(), REPO_OWN, REPO, REPO_OWN.lower(), REPO)
+    - ./syncannex %s/%s > %s_%s.log""" % (DIR_DOI_PREP, REPO_OWN.lower(), REPO_OWN,
+                                          REPO, REPO_OWN.lower(), REPO)
     fip.write(text_block)
 
     text_block = """
@@ -102,15 +100,14 @@ def print_part_pre_doi(fip):
     -[ ] sudo gin git status
     -[ ] sudo gin git remote -v
     -[ ] sudo gin git tag 10.12751/g-node.%s
-    -[ ] sudo gin git push --tags origin
-    """ % (DIR_DOI_PREP, REPO.lower(), REG_ID)
+    -[ ] sudo gin git push --tags origin""" % (DIR_DOI_PREP, REPO.lower(), REG_ID)
     fip.write(text_block)
 
     text_block = """
 -[ ] cleanup directory once tagging is done
     -[ ] sudo rm %s/%s -r
-    -[ ] sudo mv %s/%s*.log /home/%s/logs/
-    """ % (DIR_DOI_PREP, REPO.lower(), DIR_DOI_PREP, REPO_OWN.lower(), SERVER_USER)
+    -[ ] sudo mv %s/%s*.log /home/%s/logs/""" % (DIR_DOI_PREP, REPO.lower(), DIR_DOI_PREP,
+                                                 REPO_OWN.lower(), SERVER_USER)
     fip.write(text_block)
 
     text_block = """
@@ -124,8 +121,7 @@ Hi Thomas,
 the repository should be prepared for the DOI registration.
 
 Best,
-%s
-    """ % (ADMIN_NAME.split()[0])
+%s""" % (ADMIN_NAME.split()[0])
     fip.write(text_block)
 
 
@@ -143,8 +139,8 @@ def print_part_pre_doi_full(fip):
 -[ ] check the datacite content at https://gin.g-node.org/%s/%s
     - title is useful and has no typos
     - license title, license content and license link match
--[ ] log onto https://gin.g-node.org using the "doi" user (check G-Node vault for pw)
-    """ % (GIN_SERVER, REPO_OWN, REPO_OWN, REPO)
+-[ ] log onto https://gin.g-node.org using the "doi" user (check G-Node vault for pw)""" % \
+                 (GIN_SERVER, REPO_OWN, REPO_OWN, REPO)
     fip.write(text_block)
 
     text_block = """
@@ -155,9 +151,8 @@ def print_part_pre_doi_full(fip):
      use CTRL+a+d to switch out of screen sessions
     - screen -S %s
     - sudo su root
-    - ./syncannex %s/%s > %s-%s.log
-    """ % (REPO_OWN, REPO, DOI_SERVER, DIR_DOI_PREP, REPO_OWN.lower(),
-           REPO_OWN, REPO, REPO_OWN.lower(), REPO)
+    - ./syncannex %s/%s > %s-%s.log""" % (REPO_OWN, REPO, DOI_SERVER, DIR_DOI_PREP,
+                                          REPO_OWN.lower(), REPO_OWN, REPO, REPO_OWN.lower(), REPO)
     fip.write(text_block)
 
     text_block = """
@@ -165,8 +160,8 @@ def print_part_pre_doi_full(fip):
     - screen -S %s
     - sudo su root
     - sudo ./makezip %s
--[ ] sudo mv %s.zip %s/10.12751/g-node.%s/10.12751_g-node.%s.zip
-    """ % (REPO_OWN.lower(), REPO.lower(), REPO.lower(), DIR_DOI, REG_ID, REG_ID)
+-[ ] sudo mv %s.zip %s/10.12751/g-node.%s/10.12751_g-node.%s.zip""" % \
+                 (REPO_OWN.lower(), REPO.lower(), REPO.lower(), DIR_DOI, REG_ID, REG_ID)
     fip.write(text_block)
 
     text_block = """
@@ -179,16 +174,14 @@ def print_part_pre_doi_full(fip):
     -[ ] sudo gin git push --tags origin
 -[ ] clean up directory once tagging is done
     -[ ] sudo rm %s/%s -r
-    -[ ] sudo mv %s/%s*.log /home/%s/logs/
-    """ % (DIR_DOI_PREP, REPO.lower(), REG_ID,
-           DIR_DOI_PREP, REPO.lower(), DIR_DOI_PREP, REPO_OWN, SERVER_USER)
+    -[ ] sudo mv %s/%s*.log /home/%s/logs/""" % (DIR_DOI_PREP, REPO.lower(), REG_ID, DIR_DOI_PREP,
+                                                 REPO.lower(), DIR_DOI_PREP, REPO_OWN, SERVER_USER)
     fip.write(text_block)
 
     text_block = """
 -[ ] edit %s/10.12751/g-node.%s/doi.xml file 
      to include the actual size, the proper title and the proper license
-- move to local, create index.html from published doi.xml
-    """ % (DIR_DOI, REG_ID)
+- move to local, create index.html from published doi.xml""" % (DIR_DOI, REG_ID)
     fip.write(text_block)
 
     text_block = """
@@ -197,16 +190,15 @@ def print_part_pre_doi_full(fip):
     scp index.html %s@%s:/home/%s/staging
     sudo mv index.html %s/10.12751/g-node.%s/index.html
     sudo chown root:root %s
-    sudo chmod ugo+rX -R %s
-    """ % (REG_ID, SERVER_USER, DOI_SERVER, SERVER_USER, DIR_DOI, REG_ID, DIR_DOI, DIR_DOI)
+    sudo chmod ugo+rX -R %s""" % (REG_ID, SERVER_USER, DOI_SERVER, SERVER_USER,
+                                  DIR_DOI, REG_ID, DIR_DOI, DIR_DOI)
     fip.write(text_block)
 
     text_block = """
 - https://doi.gin.g-node.org/10.12751/g-node.%s
     -[ ] check page access, size, title, license name
     -[ ] check all links
-    -[ ] check zip download and suggested size
-    """ % REG_ID
+    -[ ] check zip download and suggested size""" % REG_ID
     fip.write(text_block)
 
     text_block = """
@@ -225,8 +217,7 @@ In the doi.xml the following changes were made and the index.html page has been 
 - Zip size was added.
 
 Best,
-%s
-    """ % (ADMIN_NAME.split()[0])
+%s""" % (ADMIN_NAME.split()[0])
     fip.write(text_block)
 
     text_block = """
@@ -250,8 +241,8 @@ def print_part_post_doi(fip):
                             <br>%s</td>
                             <td>%s</td>
                             <td><a href="https://doi.org/10.12751/g-node.%s" class ="ui grey label">10.12751/g-node.%s</a></td>
-                        </tr>
-    """ % (DOI_SERVER, DIR_DOI, REG_ID, TITLE, CITATION, REG_DATE, REG_ID, REG_ID)
+                        </tr>""" % (DOI_SERVER, DIR_DOI, REG_ID, TITLE, CITATION,
+                                    REG_DATE, REG_ID, REG_ID)
     fip.write(text_block)
 
     text_block = """
@@ -260,8 +251,8 @@ def print_part_post_doi(fip):
 -[ ] gin get G-Node/DOImetadata to local staging directory
 -[ ] create empty "keywords" directory and run the following from it
 -[ ] /path/to/gindoid make-keyword-pages /path/to/DOImetadata/*.xml
--[ ] scp -r keywords %s@%s:/home/%s/staging
-    """ % (DIR_DOI, REG_ID, SERVER_USER, DOI_SERVER, SERVER_USER)
+-[ ] scp -r keywords %s@%s:/home/%s/staging""" % (DIR_DOI, REG_ID, SERVER_USER,
+                                                  DOI_SERVER, SERVER_USER)
     fip.write(text_block)
 
     text_block = """
@@ -270,8 +261,7 @@ def print_part_post_doi(fip):
 -[ ] sudo mv %s/keywords %s/keywords_
 -[ ] sudo mv keywords/ %s
 -[ ] check landing page and keywords online: https://doi.gin.g-node.org
--[ ] sudo rm %s/keywords_ -r
-    """ % (DOI_SERVER, DIR_DOI, DIR_DOI, DIR_DOI, DIR_DOI)
+-[ ] sudo rm %s/keywords_ -r""" % (DOI_SERVER, DIR_DOI, DIR_DOI, DIR_DOI, DIR_DOI)
     fip.write(text_block)
 
     text_block = """
@@ -285,8 +275,7 @@ def print_part_post_doi(fip):
     sudo chattr +i %s/10.12751/g-node.%s/10.12751_g-node.%s.zip
 -[ ] cleanup any leftover directories from previous versions of this dataset
 -[ ] email to user (check below)
--[ ] close all related issues on DOImetadata
-    """ % (DIR_DOI, REG_ID, REG_ID, DIR_DOI, REG_ID, REG_ID)
+-[ ] close all related issues on DOImetadata""" % (DIR_DOI, REG_ID, REG_ID, DIR_DOI, REG_ID, REG_ID)
     fip.write(text_block)
 
 
@@ -324,8 +313,8 @@ The latter will result in a link in the Datacite database to your publication an
 
 Best regards,
   %s
-  German Neuroinformatics Node
-    """ % (EMAIL, REPO_OWN, REPO, USER_FULL_NAME, TITLE, REG_ID, REG_ID, ADMIN_NAME)
+  German Neuroinformatics Node""" % (EMAIL, REPO_OWN, REPO, USER_FULL_NAME,
+                                     TITLE, REG_ID, REG_ID, ADMIN_NAME)
     fip.write(text_block)
 
 
