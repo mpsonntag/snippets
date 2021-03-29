d116df0ba57f44b9615077e62ff4a7cc0e5b5d29
M. Sonntag <michael.p.sonntag@gmail.com>
2021-03-29 20:04:40 +0200

    [py/doi] Add pre_fork_sync func

--- python/DOI/doichecklist_02.py
+++ python/DOI/doichecklist_02.py
@@ -105,6 +105,23 @@ def text_pre_fork_upload(screen_id):
     return text_block
 
 
+def text_pre_fork_sync(screen_id):
+    logfile = f"""{CONF["repo_own"].lower()}-{CONF["repo"].lower()}.log"""
+
+    text_block = f"""
+
+-[ ] log on to the DOI server ({CONF["doi_server"]}) and move to {CONF["dir_doi_prep"]}
+-[ ] fetch git and annex content and upload annex content to the DOI fork repo.
+     use screen to avoid large down- and uploads to be interrupted.
+     use CTRL+a+d to switch out of screen sessions without interruption.
+     use either the logfile or 'htop' to check on the status of the download/upload.
+    - screen -S {screen_id}
+    - sudo su root
+    - ./syncannex {CONF["repo_own"]}/{CONF["repo"]} > {logfile}"""
+
+    return text_block
+
+
 def text_pre_git_tag():
     text_block = f"""
 
@@ -240,7 +257,7 @@ def print_part_pre_doi_full(fip):
     fip.write(text_block)
 
     screen_id = f"{CONF['repo_own'].lower()}-{str(uuid4())[0:5]}"
-    text_block = text_pre_fork_upload(screen_id)
+    text_block = text_pre_fork_sync(screen_id)
     fip.write(text_block)
 
     text_block = f"""
