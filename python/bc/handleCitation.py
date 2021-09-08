024b438c2138c59d200a9149bec52a7276d71f04
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-08 11:33:57 +0200

    [py/bc] Handle citation middle name

--- python/bc/handleCitation.py
+++ python/bc/handleCitation.py
@@ -8,7 +8,9 @@ with open(fname) as jfp:
 for item in data:
     cit_list = ""
     for auth in item["authors"]:
-        first_name = f" {auth['firstName'][0]}." if len(auth["firstName"]) > 0 else ""
-        citation = f"{auth['lastName']}{first_name}"
+        first_name = f" {auth['firstName'][0]}." if "firstName" in auth.keys() and len(auth["firstName"]) > 0 else ""
+        mid_name = ""
+        if "middleName" in auth.keys() and auth["middleName"] and len(auth["middleName"]) > 0:
+            mid_name = f" {auth['middleName'][0]}."
+        citation = f"{auth['lastName']}{first_name}{mid_name}"
         cit_list = f"{cit_list}, {citation}" if len(cit_list) > 0 else citation
-    print(cit_list)
