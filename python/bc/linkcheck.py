9d8ae7224a602255cae9d7b14b1640427dd1a53e
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-21 10:31:34 +0200

    [py/bc/linkcheck] Add file

---
+++ python/bc/linkcheck.py
@@ -0,0 +1,15 @@
+"""
+Check all provided links from a poster gallery json file
+for non http.StatusOK codes.
+"""
+
+
+def main():
+    """
+    Parse command line arguments and run the URL checks with the data provided.
+    """
+    pass
+
+
+if __name__ == "main":
+    main()
