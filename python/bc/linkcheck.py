6751c3987d7b2877e61b0fcdfcad08b4bcf7952c
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-21 10:52:56 +0200

    [py/bc/linkcheck] Add content sanity checks

--- python/bc/linkcheck.py
+++ python/bc/linkcheck.py
@@ -9,7 +9,29 @@ from typing import Dict, List
 
 
 def handle_poster(data: List[Dict[str, str]]):
-    print(data)
+    """
+    Iterate over all provided poster items and check the http.StatusOK for
+    every full and derivative link item.
+    :param data: list containing poster dictionary items.
+    """
+
+
+def content_check(data: List[Dict[str, str]], check: str,
+                  json_file: str, msg: str) -> bool:
+    """
+    Checks whether a dictionary contains a column specific to expected data and returns
+    a corresponding boolean value. This avoids writing files of an invalid gallery type.
+    :param data: list containing poster dictionary items.
+    :param check: name of the column that is supposed to be found in the dictionary items.
+    :param json_file: name of the file containing the data
+    :param msg: String displayed in command line print.
+    :return: True if the column was found in the data, False otherwise.
+    """
+    if not data or check not in data[0].keys():
+        print(f"'{json_file}' does not seem to be a valid {msg} file ...")
+        print("Aborting ...")
+        return False
+    return True
 
 
 def main():
@@ -33,13 +55,22 @@ def main():
 
     if workshops:
         print("Handling workshops data ...")
+        if not content_check(data, "workshop number", json_file, "WORKSHOP"):
+            return
+
         return
 
     if exhibition:
         print("Handling exhibition data ...")
+        if not content_check(data, "company_name", json_file, "EXHIBITION"):
+            return
+
         return
 
     print("Handling poster data ...")
+    if not content_check(data, "abstract_number", json_file, "POSTERS"):
+        return
+
     handle_poster(data)
 
 
