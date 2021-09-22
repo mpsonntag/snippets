a74de1d8099da6f96dbb99a31f146cd5d910de00
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-22 17:00:48 +0200

    [py/bc/access_stats] Handle docker logs json file

--- python/bc/access_stats.py
+++ python/bc/access_stats.py
@@ -1,13 +1,24 @@
 """
 Extract infos from docker json logfiles
 """
+import argparse
+import json
 
 
 def main():
     """
     Parse command line arguments and run the URL checks with the data provided.
     """
-    pass
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("json_file", help="Docker logs JSON file")
+    args = parser.parse_args()
+
+    json_file = args.json_file
+    with open(json_file, "r", encoding="utf-8") as jfp:
+        data_string = jfp.read().replace('}\n{', '},\n{')
+        data = json.loads(f"[{data_string}]")
+
+    print(len(data))
 
 
 if __name__ == "__main__":
