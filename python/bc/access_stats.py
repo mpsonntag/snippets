9f0a52f8228f426dcae1dd8cb9bf2d024e2458e4
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-22 16:47:46 +0200

    [py/bc] Add access stats file

---
+++ python/bc/access_stats.py
@@ -0,0 +1,14 @@
+"""
+Extract infos from docker json logfiles
+"""
+
+
+def main():
+    """
+    Parse command line arguments and run the URL checks with the data provided.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
