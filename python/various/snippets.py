4956a88fd46b1bd9572d5ea8b6c46d5b58034bf1
M. Sonntag <michael.p.sonntag@gmail.com>
2021-10-01 21:28:42 +0200

    [py/various/snippets] Add ticket calc

--- python/various/snippets.py
+++ python/various/snippets.py
@@ -20,6 +20,17 @@ def handle_kwargs(filename, **kwargs):
     conditional_print(filename, local_style, custom_template)
 
 
+def scale_print(add_trips=0):
+    curr = 1440 + 120+60
+    pot = 949 + 600
+    if not add_trips:
+        print(f"Current: {curr}\tklimaticket: {pot}")
+    for i in range(add_trips):
+        curr_add = i * (120 + 10)
+        pot_add = i * 50
+        print(f"Current: {curr + curr_add}\tklimaticket: {pot + pot_add}\tadd trip {i}")
+
+
 # Test all combinations
 handle_kwargs("I am all alone")
 handle_kwargs("I have a naughty friend", invalid_a=False, invalid_b=None, invalid_c="naughty")
