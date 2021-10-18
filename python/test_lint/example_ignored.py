42b4222bbd954fddfae36fa1d1b7264d3ce322bd
M. Sonntag <michael.p.sonntag@gmail.com>
2021-10-18 20:16:26 +0200

    [notebooks/202110 RTG] Add basic test description

---
+++ python/test_lint/example_ignored.py
@@ -0,0 +1,6 @@
+def test_two():
+    assert "a" == "b"
+
+
+def test_three():
+    assert 1 == 1
---
+++ python/test_lint/test_example.py
@@ -0,0 +1,5 @@
+def some_more_test():
+    assert "a" == "b"
+
+def test_something_else():
+    assert 1 == 1
