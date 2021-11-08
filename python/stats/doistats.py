ca774cd4782025487b049f833b4209fd0c55b56c
M. Sonntag <michael.p.sonntag@gmail.com>
2021-11-08 13:53:20 +0100

    [py/stats/doi] Parse dates list

--- python/stats/doistats.py
+++ python/stats/doistats.py
@@ -5,7 +5,12 @@ DOI_MAIN_URL = "https://doi.gin.g-node.org/index.html"
 
 def main():
     content = requests.get(DOI_MAIN_URL)
-    print(content.text)
+    contlist = content.text.splitlines()
+    dates_list = []
+    for line in contlist:
+        if "<td>" in line and "-" in line and not "<a" in line:
+            dates_list.append(line.strip().replace("<td>", "").replace("</td>", ""))
+    print(dates_list)
 
 
 if __name__ == "__main__":
