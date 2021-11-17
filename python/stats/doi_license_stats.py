80571e11a80b847bb820715ddec0d5e4051b846a
M. Sonntag <michael.p.sonntag@gmail.com>
2021-11-17 10:43:33 +0100

    [py/stats] Add doi license script

---
+++ python/stats/doi_license_stats.py
@@ -0,0 +1,15 @@
+"""
+Get statistics on used license names from published G-Node
+datasets. Requires a local copy of the DOImetadata repository.
+"""
+
+
+def main():
+    """
+    Handle commandline arguments, call analysis procedures and handle statistics results.
+    """
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
