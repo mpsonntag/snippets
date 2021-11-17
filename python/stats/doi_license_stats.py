b5d7bc2f757108410f235303a9bc925a19d3a521
M. Sonntag <michael.p.sonntag@gmail.com>
2021-11-17 10:55:59 +0100

    [py/stats/license stats] Add argparse

--- python/stats/doi_license_stats.py
+++ python/stats/doi_license_stats.py
@@ -2,13 +2,22 @@
 Get statistics on used license names from published G-Node
 datasets. Requires a local copy of the DOImetadata repository.
 """
+import argparse
+
+
+def license_names_from_files(dir_path: str):
+    print(dir_path)
 
 
 def main():
     """
     Handle commandline arguments, call analysis procedures and handle statistics results.
     """
-    print("Done")
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("dir_path", help="Path to the DOImetadata directory")
+    args = parser.parse_args()
+
+    license_names_from_files(args.dir_path)
 
 
 if __name__ == "__main__":
