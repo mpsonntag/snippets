c85c20522b748f750e34deac5433f362a5811fec
M. Sonntag <michael.p.sonntag@gmail.com>
2021-12-08 19:02:04 +0100

    [cmd/tickexp] Add results page

--- cmd/tickexp/serv.go
+++ cmd/tickexp/serv.go
@@ -6,3 +6,38 @@ type ExpItem struct {
 	value string
 	desc  string
 }
+
+// ResultsPage renders the results
+const ResultsPage = `<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="robots" content="noindex,nofollow">
+		<meta name="author" content="miso">
+		<meta name="description" content="Info">
+		<title>tickexp results</title>
+	</head>
+	<body>
+		<h1>Tickexp results page</h1>
+
+		<p>Le grande total: 949-[value]</p>
+
+		<table>
+			<thead>
+				<tr>
+					<th>Date</th>
+					<th>Value</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	</body>
+</html>`
