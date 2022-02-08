44f92e66a65ec1d9123d2a2ba804e78b535f8857
M. Sonntag <michael.p.sonntag@gmail.com>
2022-02-08 16:59:56 +0100

    [cmd/annexcheck] Add main file

---
+++ cmd/annexcheck/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func runannexcheck() {
+	fmt.Println("Running annexcheck")
+}
+
+func setUpCommands(verstr string) *cobra.Command {
+	var rootCmd = &cobra.Command{
+		Use:                   "annexcheck",
+		Long:                  "annexcheck",
+		Version:               fmt.Sprintln("0.0.1"),
+		DisableFlagsInUseLine: true,
+	}
+	cmds := make([]*cobra.Command, 1)
+	cmds[0] = &cobra.Command{
+		Use:                   "run",
+		Short:                 "run the annexcheck from the current directory",
+		Args:                  cobra.NoArgs,
+		Run:                   runannexcheck,
+		Version:               fmt.Sprintln("0.0.1"),,
+		DisableFlagsInUseLine: true,
+	}
+
+	rootCmd.AddCommand(cmds...)
+
+	return rootCmd
+}
+
+func main() {
+	fmt.Println("...[I] parsing CLI")
+
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Printf("...[E] running tickexp: %q\n", err.Error())
+	}
+}
