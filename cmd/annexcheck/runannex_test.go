8d9d1ffdd7bd4ba583e20ca152d673c6ffb6f72f
M. Sonntag <michael.p.sonntag@gmail.com>
2022-02-10 18:50:44 +0100

    [cmd/annexcheck] Add runannexcheck test stub

--- cmd/annexcheck/runannex_test.go
+++ cmd/annexcheck/runannex_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -36,4 +38,67 @@ func TestGitCMD(t *testing.T) {
 	} else if stderr != "" {
 		t.Fatalf("command error running git command: %q", stderr)
 	}
+}
+
+func TestRunannexcheck(t *testing.T) {
+	targetpath, err := ioutil.TempDir("", "test_runannexcheck")
+	if err != nil {
+		t.Fatalf("Failed to create temp directory: %v", err)
+	}
+	defer os.RemoveAll(targetpath)
+
+	// we are playing around with os paths here which can get nasty.
+	// add full tests for directory switching to ensure the server
+	// will not get confused
+
+	// check curr dir
+	//currdir, err := os.Getwd()
+	//if err != nil {
+	// change to path of the executable if possible and check again
+	//path, err := os.Executable()
+	//fmt.Printf("Executable path: %s", path)
+	//if err != nil {
+	//	t.Fatalf("Could not assert executable path: %s, %s", err.Error(), path)
+	//	}
+	//		err = os.Chdir(path)
+	//		if err != nil {
+	//t.Fatalf("Path issues arose: %s", err.Error())
+	//		}
+	//	}
+	//currdir, err = os.Getwd()
+	//if err != nil {
+	//t.Fatalf("still issues with curr dir: %s", err.Error())
+	//}
+	//fmt.Printf("current dir A: %q\n", currdir)
+
+	// test non existing directory
+	repodir := "/home/not/exist"
+	out, err := runannexcheck(repodir)
+	fmt.Printf("Running non existing dir check: %q, %q", out, err.Error())
+	if err == nil || !strings.Contains(err.Error(), "path not found") {
+		// check directory after function
+		t.Fatalf("missing error on invalid dir path: %q\n%s", out, err.Error())
+	}
+
+	// test non-git dir
+	_, err = runannexcheck(targetpath)
+	fmt.Printf("Running non existing dir check: %q", err.Error())
+	if err == nil {
+		t.Fatalf("missing error on non-git dir: %q\n%s", out, err.Error())
+	}
+	currdir, direrr := os.Getwd()
+	if direrr != nil {
+		t.Fatalf("Could not assert current directory: %s", direrr.Error())
+	}
+	fmt.Printf("current dir B: %q\n", currdir)
+
+	// test non-git annex dir
+
+	// test valid git, missing annex content
+
+	// test valid git, locked annex content
+
+	// test valid git, no annex content
+
+	// test valid git, valid annex content
 }
