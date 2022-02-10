e0a89b20373dc5282a4363ed3db60905ab3da539
M. Sonntag <michael.p.sonntag@gmail.com>
2022-02-10 18:50:24 +0100

    [cmd/annexcheck] Add gitCMD test

--- cmd/annexcheck/runannex_test.go
+++ cmd/annexcheck/runannex_test.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestSimpleRun(t *testing.T) {
-	repodir := "/home/sommer/Chaos/DL/annextmp"
-	_, _, err := runannexcheck(repodir)
+func TestGitCMD(t *testing.T) {
+	targetpath, err := ioutil.TempDir("", "test_gitcmd")
 	if err != nil {
-		t.Fatalf("I think we got something: %s", err.Error())
+		t.Fatalf("Failed to create temp directory: %v", err)
+	}
+	defer os.RemoveAll(targetpath)
+
+	// test non existing directory error
+	_, _, err = gitCMD("/home/not/exist", "")
+	if err == nil {
+		t.Fatal("non existing directory should return an error")
+	}
+
+	// test non git directory error
+	_, _, err = gitCMD(targetpath, "status")
+	if err == nil {
+		t.Fatal("non git directory should return an error")
+	}
+
+	// test valid git command
+	_, _, err = gitCMD(targetpath, "init")
+	if err != nil {
+		t.Fatalf("error on initializing git dir: %q", err.Error())
+	}
+	_, stderr, err := gitCMD(targetpath, "status")
+	if err != nil {
+		t.Fatal("error on running git command")
+	} else if stderr != "" {
+		t.Fatalf("command error running git command: %q", stderr)
 	}
 }
