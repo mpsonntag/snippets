fa07f62522076a1d4afe9a510e3d25a8bf8c0873
M. Sonntag <michael.p.sonntag@gmail.com>
2022-02-24 10:03:05 +0100

    [cmd/annexcheck] Add lockedAnnexContent func

--- cmd/annexcheck/main.go
+++ cmd/annexcheck/main.go
@@ -94,6 +94,24 @@ func missingAnnexContent(gitdir string) (bool, string, error) {
 	return true, string(stdout), nil
 }
 
+func lockedAnnexContent(gitdir string) (bool, string, error) {
+	if _, err := os.Stat(gitdir); os.IsNotExist(err) {
+		return false, "", fmt.Errorf("path not found %q", gitdir)
+	}
+	// command should not return with an error or with any stderr content
+	// If stdout is empty, there is no locked content. If it is not empty,
+	// the number of lines correspond to the number of files with locked content.
+	stdout, stderr, err := remoteGitCMD(gitdir, true, "find", "--locked")
+	if err != nil {
+		return false, "", err
+	} else if string(stderr) != "" {
+		return false, "", fmt.Errorf("git annex error: %s", string(stderr))
+	} else if string(stdout) == "" {
+		return false, "", nil
+	}
+	return true, string(stdout), nil
+}
+
 func checkAnnexComplete(repopath string) (gitrepoinfo, error) {
 	log.Printf("Start annex check for repo %q", repopath)
 
