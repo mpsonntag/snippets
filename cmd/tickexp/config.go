ee80f16b2b457e4015ae6bd0f7476e0e7d55e65c
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-11 13:03:25 +0100

    [cmd/tickexp] Add readConf func

--- cmd/tickexp/config.go
+++ cmd/tickexp/config.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"os"
+)
+
 type Configuration struct {
 	Port uint16
 	User string
 	Phrase string
 	Expiration int
+}
+
+// ReadConf returns the value of a configuration env variable.
+// If the variable is not set, an empty string is returned (ignores any errors).
+func readConf(key string) string {
+	value, _ := os.LookupEnv(key)
+	return value
 }
