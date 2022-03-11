9e33f1b93bde8187f7cf187290ac3128785ff3bf
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-11 13:03:59 +0100

    [cmd/tickexp] Add loadConf func

--- cmd/tickexp/config.go
+++ cmd/tickexp/config.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 )
 
 type Configuration struct {
@@ -16,4 +18,40 @@ type Configuration struct {
 func readConf(key string) string {
 	value, _ := os.LookupEnv(key)
 	return value
+}
+
+func loadconfig() (Configuration, error) {
+	cfg := Configuration{}
+
+	port := uint16(8899)
+	portstr := readConf("port")
+	if portstr != "" {
+		portint, err := strconv.ParseUint(portstr, 10, 16)
+		if err != nil {
+			fmt.Printf("Could not parse port %q, using default %d", portstr, port)
+		} else {
+			port = uint16(portint)
+		}
+	}
+	cfg.Port = port
+
+	cfg.User = readConf("user")
+	cfg.Phrase = readConf("phrase")
+	if cfg.User == "" || cfg.Phrase == "" {
+		return cfg, fmt.Errorf("missing user (%s) or phrase (%s)", cfg.User, cfg.Phrase)
+	}
+
+	exp := 1
+	expstr := readConf("expiration")
+	if expstr != "" {
+		expint, err := strconv.Atoi(expstr)
+		if err != nil {
+			fmt.Printf("Could not parse expiration %q, using default %d", expstr, expint)
+		} else {
+			exp = expint
+		}
+	}
+	cfg.Expiration = exp
+
+	return cfg, nil
 }
