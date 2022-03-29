646a92225a971e6be80b37b66ee237e0b50248c3
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-29 18:16:47 +0200

    [cmd/missingcontent] Add remoteAnnexCmd func

--- cmd/missingcontent/main.go
+++ cmd/missingcontent/main.go
@@ -1,9 +1,35 @@
 package main
 
-import(
+import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/G-Node/gin-cli/git/shell"
 )
 
-func main() {
-	fmt.Println("Starting checks")
+// AnnexCommand sets up a git annex command with the provided arguments and returns a GinCmd struct.
+func remoteAnnexCommand(annexbinpath string, gitdir string, args ...string) (shell.Cmd, error) {
+	if _, err := os.Stat(annexbinpath); os.IsNotExist(err) {
+		return shell.Cmd{}, fmt.Errorf("annex path not found %q", annexbinpath)
+	}
+	if _, err := os.Stat(gitdir); os.IsNotExist(err) {
+		return shell.Cmd{}, fmt.Errorf("gitdir path not found %q", gitdir)
+	}
+	gitannexpath := annexbinpath
+
+	cmdstr := append([]string{"-C", gitdir, "annex"}, args...)
+	cmd := shell.Command("git", cmdstr...)
+
+	// make sure the local annex is available to the command
+	env := os.Environ()
+	syspath := os.Getenv("PATH")
+	syspath += string(os.PathListSeparator) + gitannexpath
+	cmd.Env = append(env, syspath)
+
+	workingdir, _ := filepath.Abs(".")
+	fmt.Printf("Running shell command (Dir: %s): %s", workingdir, strings.Join(cmd.Args, " "))
+
+	return cmd, nil
 }
