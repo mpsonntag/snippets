1e711d1ebd77e0d9a9c1fe52b06fc2a4b9465030
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-30 15:08:58 +0200

    [cmd/misscont] AnnexCheck gitdir independent

--- cmd/misscont/main.go
+++ cmd/misscont/main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/G-Node/gin-cli/git/shell"
 )
 
-// AnnexCommand sets up a git annex command with the provided arguments and returns a GinCmd struct.
-func remoteAnnexCommand(annexbinpath string, gitdir string, args ...string) (shell.Cmd, error) {
+// annexCommand sets up a git annex command with the provided arguments and returns a GinCmd struct.
+func annexCommand(annexbinpath string, gitdir string, args ...string) (shell.Cmd, error) {
 	if _, err := os.Stat(annexbinpath); os.IsNotExist(err) {
 		return shell.Cmd{}, fmt.Errorf("annex path not found %q", annexbinpath)
 	}
@@ -37,8 +37,8 @@ func remoteAnnexCommand(annexbinpath string, gitdir string, args ...string) (she
 // annexCMD runs the passed git annex command arguments.
 // The command returns stdout and stderr as strings and any error that might occur.
 func annexCMD(annexbinpath string, gitdir string, annexargs ...string) (string, string, error) {
-	fmt.Printf("Running annex command: %s\n", annexargs)
-	cmd, err := remoteAnnexCommand(annexbinpath, gitdir, annexargs...)
+	fmt.Printf("[I] running annex cmd: %s\n", annexargs)
+	cmd, err := annexCommand(annexbinpath, gitdir, annexargs...)
 	if err != nil {
 		return "", "", err
 	}
@@ -51,8 +51,9 @@ func annexCMD(annexbinpath string, gitdir string, annexargs ...string) (string, 
 // The function returns false with no error, if the annex command execution
 // ends with the git message that 'annex' is not a git command.
 // It will return false and the error message on any different error.
-func annexAvailable(annexbinpath string, gitdir string) (bool, error) {
-	_, stderr, err := annexCMD(annexbinpath, gitdir, "version")
+func annexAvailable(annexbinpath string) (bool, error) {
+	_, stderr, err := annexCMD(annexbinpath, "", "version")
+	fmt.Printf("%q, %v\n", stderr, err)
 	if err != nil {
 		if strings.Contains(stderr, "'annex' is not a git command") {
 			return false, nil
@@ -85,7 +86,7 @@ func main() {
 	fmt.Printf("[I] using annexpath %q\n", annexpath)
 
 	// check if annex is available; exit otherwise
-	ok, err := annexAvailable(annexpath, "")
+	ok, err := annexAvailable(annexpath)
 	if err != nil {
 		fmt.Printf("[E] checking annex: %s\n", err.Error())
 		os.Exit(1)
