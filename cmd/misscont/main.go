2f9e668782a8aecd93da9dbe6f004fcda4dafc73
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-30 17:53:18 +0200

    [cmd/misscont] Add handlebinpath func

--- cmd/misscont/main.go
+++ cmd/misscont/main.go
@@ -194,6 +194,34 @@ func checkgitdirs(cmd *cobra.Command, args []string) {
 	}
 }
 
+func handlebinpath() (string, error) {
+	binpath, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("[E] fetching executable path: %s", err.Error())
+	}
+	if _, err := os.Stat(binpath); os.IsNotExist(err) {
+		return "", fmt.Errorf("[E] executable path could not be identified: %s", err.Error())
+	}
+
+	binname := filepath.Base(binpath)
+	annexpath := strings.Replace(binpath, binname, "git-annex.linux", 1)
+
+	if _, err := os.Stat(annexpath); err != nil {
+		return "", fmt.Errorf("[E] annex binary path %q not found: %s", annexpath, err.Error())
+	}
+
+	// check if annex is available; exit otherwise
+	ok, err := annexAvailable(annexpath)
+	if err != nil {
+		return "", fmt.Errorf("[E] checking annex at %q: %s", annexpath, err.Error())
+	}
+
+	if !ok {
+		return "", fmt.Errorf("[E] annex is not available at %q", annexpath)
+	}
+	return annexpath, nil
+}
+
 // main checks git annex availability and runs the git annex repo statistics with the provided directory tree.
 // TODO not sure how to best handle annex; currently it expects the binary to be in the same directory as the
 // annex content directory; in any case, the user running the binary has to have a home directory for annex to
