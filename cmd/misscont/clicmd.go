b2fd7f5839a35b56fd33c7a60f3b4bd84c0c71f4
M. Sonntag <michael.p.sonntag@gmail.com>
2022-03-31 19:04:31 +0200

    [cmd/misscont] func walkgitdirs to RepoInfo method

--- cmd/misscont/clicmd.go
+++ cmd/misscont/clicmd.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -24,7 +25,69 @@ type RepoInfoCMD struct {
 	// annex binaries locally if it is not provided by the host
 	LocalAnnexPath string
 }
+
+func (repin *RepoInfoCMD) walkgitdirs(dirpath string) error {
+	if !repin.AnnexAvailable {
+		return fmt.Errorf("[E] could not verify annex walking dir %q", dirpath)
+	}
+
+	gitlist := []string{}
+	annexInfoList := []AnnexInfo{}
+	currwalk := func(currpath string, fio os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("[E] encountered filepath walk at %s error: %q\n", currpath, err.Error())
+			return err
+		}
+		// ignore files
+		if !fio.IsDir() {
+			fmt.Printf("[I] Ignoring file %q\n", currpath)
+			return filepath.SkipDir
+		}
+
+		// check if the current directory is a git directory, bare or otherwise
+		// assume the current directory is a bare git repo. stop and do all check stuff required
+		if strings.HasSuffix(currpath, ".git") {
+			fmt.Printf("[I] current dir %q is a bare repo; skipping\n", currpath)
+
+			aninf, err := infoFromBareRepo(repin.LocalAnnexPath, currpath)
+			if err != nil {
+				fmt.Printf("[E] handling bare repo info: %q", err.Error())
+				return err
+			}
+
+			annexInfoList = append(annexInfoList, aninf)
+			gitlist = append(gitlist, currpath)
+			return filepath.SkipDir
+		}
+		// check if the current directory is a normal git dir. stop and do all check stuff required
+		checkgitpath := filepath.Join(currpath, ".git")
+		inf, err := os.Stat(checkgitpath)
+		if err != nil && !os.IsNotExist(err) {
+			fmt.Printf("[I] err checking git dir, continuer: %q\n", err.Error())
+		} else if os.IsNotExist(err) {
+			fmt.Println("[I] curr dir has no .git folder, continue")
+		} else if inf.IsDir() {
+			fmt.Printf("[I] current dir %q is a git dir; skipping\n", currpath)
+			gitlist = append(gitlist, currpath)
+			return filepath.SkipDir
+		}
+
+		fmt.Printf("[I] curr dir %q is no git dir, moving on\n", currpath)
+		return nil
+	}
+
+	err := filepath.Walk(dirpath, currwalk)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[I] Found the following git dirs: %v\n", gitlist)
+	fmt.Printf("[I] Annexinfo: %v\n", annexInfoList)
+	return nil
+}
+
 func repoinfocmd(cmd *cobra.Command, args []string) {
+	// handle command arguments
 	dirpath, err := cmd.Flags().GetString("checkdir")
 	if err != nil {
 		fmt.Printf("[E] parsing directory flag: %s\n", err.Error())
@@ -38,10 +101,11 @@ func repoinfocmd(cmd *cobra.Command, args []string) {
 		fmt.Printf("[E] could not find directory %q, %s\n", gitdirs, err.Error())
 		os.Exit(1)
 	}
-
 	fmt.Printf("[I] using directory %q\n", gitdirs)
 
-	err = walkgitdirs("", gitdirs)
+	// walk the directory tree to collect git repository information
+	collector := RepoInfoCMD{}
+	err = collector.walkgitdirs(gitdirs)
 	if err != nil {
 		fmt.Printf("[E] walking directories: %s\n", err.Error())
 	}
--- cmd/misscont/utils.go
+++ cmd/misscont/utils.go
@@ -7,62 +7,6 @@ import (
 	"strings"
 )
 
-func walkgitdirs(annexbinpath, dirpath string) error {
-	gitlist := []string{}
-	annexInfoList := []AnnexInfo{}
-	currwalk := func(currpath string, fio os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Printf("[E] encountered filepath walk at %s error: %q\n", currpath, err.Error())
-			return err
-		}
-		// ignore files
-		if !fio.IsDir() {
-			fmt.Printf("[I] Ignoring file %q\n", currpath)
-			return filepath.SkipDir
-		}
-
-		// check if the current directory is a git directory, bare or otherwise
-		// assume the current directory is a bare git repo. stop and do all check stuff required
-		if strings.HasSuffix(currpath, ".git") {
-			fmt.Printf("[I] current dir %q is a bare repo; skipping\n", currpath)
-
-			aninf, err := infoFromBareRepo(annexbinpath, currpath)
-			if err != nil {
-				fmt.Printf("[E] handling bare repo info: %q", err.Error())
-				return err
-			}
-
-			annexInfoList = append(annexInfoList, aninf)
-			gitlist = append(gitlist, currpath)
-			return filepath.SkipDir
-		}
-		// check if the current directory is a normal git dir. stop and do all check stuff required
-		checkgitpath := filepath.Join(currpath, ".git")
-		inf, err := os.Stat(checkgitpath)
-		if err != nil && !os.IsNotExist(err) {
-			fmt.Printf("[I] err checking git dir, continuer: %q\n", err.Error())
-		} else if os.IsNotExist(err) {
-			fmt.Println("[I] curr dir has no .git folder, continue")
-		} else if inf.IsDir() {
-			fmt.Printf("[I] current dir %q is a git dir; skipping\n", currpath)
-			gitlist = append(gitlist, currpath)
-			return filepath.SkipDir
-		}
-
-		fmt.Printf("[I] curr dir %q is no git dir, moving on\n", currpath)
-		return nil
-	}
-
-	err := filepath.Walk(dirpath, currwalk)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("[I] Found the following git dirs: %v\n", gitlist)
-	fmt.Printf("[I] Annexinfo: %v\n", annexInfoList)
-	return nil
-}
-
 func handlebinpath() (string, error) {
 	binpath, err := os.Executable()
 	if err != nil {
