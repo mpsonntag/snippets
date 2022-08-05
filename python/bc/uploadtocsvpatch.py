1e859091c659bcbdb032b9ab26b9883fa26a8cf2
M. Sonntag <michael.p.sonntag@gmail.com>
2022-08-05 10:33:07 +0200

    [py/bc/csvuploadkey] Read 'id' csv column

--- python/bc/uploadtocsvpatch.py
+++ python/bc/uploadtocsvpatch.py
@@ -5,6 +5,7 @@ file is saved.
 """
 
 import argparse
+import pandas as pd
 
 
 def main():
@@ -12,9 +13,17 @@ def main():
     Handles the command line arguments.
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("csv_file", help="Tab delimited CSV file containing the abstract id data")
+    parser.add_argument("csv_file", help="CSV file containing the abstract id data")
     parser.add_argument("code_salt", help="Salt string to create poster upload codes")
+    parser.add_argument("-s", default="\t",
+                        help="CSV file column separator; default is tab")
     args = parser.parse_args()
+
+    csv_file = args.csv_file
+    csv_sep = args.s
+    csv_data = pd.read_csv(csv_file, header=0, sep=csv_sep)
+
+    print(csv_data.loc[:, "id"])
 
 
 if __name__ == "__main__":
