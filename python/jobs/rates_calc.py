46762932eca9ecbb56eda4bf0286d94a23bcda87
M. Sonntag <michael.p.sonntag@gmail.com>
2023-02-09 16:43:25 +0100

    [py/jobs] Add commission_income func

--- python/jobs/rates_calc.py
+++ python/jobs/rates_calc.py
@@ -19,16 +19,20 @@ SELF_EMPLOYED_PROVISION = 1.53
 # absolute
 ACCIDENT_INSURANCE = 10.64
 
-# move this to a func
-earning = MAX_BASE_EARNING
 
-hin = (earning/100)*HEALTH_INSURANCE
-pin = (earning/100)*PENSION_INSURANCE
-sep = (earning/100)*SELF_EMPLOYED_PROVISION
+def commission_income(earning=MAX_BASE_EARNING):
+    hin = (earning / 100) * HEALTH_INSURANCE
+    pin = (earning / 100) * PENSION_INSURANCE
+    sep = (earning / 100) * SELF_EMPLOYED_PROVISION
 
-commission = hin+pin+sep+ACCIDENT_INSURANCE
-net_income = earning-commission
+    commission = hin + pin + sep + ACCIDENT_INSURANCE
+    net_income = earning - commission
 
-print(f"Commission for income {earning:.2f}")
-print(f"-- monthly commission: {commission:.2f};\t\t net income: {net_income:.2f}")
-print(f"-- commission per annum: {commission*12:.2f};\t net income: {net_income*12:.2f}")
+    print(f"Commission for income {earning:.2f}")
+    print(f"-- monthly commission: {commission:.2f};\t\t net income: {net_income:.2f}")
+    print(f"-- commission per annum: {commission * 12:.2f};\t net income: {net_income * 12:.2f}")
+
+
+commission_income()
+
+commission_income(3000)
