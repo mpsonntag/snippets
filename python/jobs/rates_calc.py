2c086d9d512bcfbe3f9cf687ec7107916d547f8b
M. Sonntag <michael.p.sonntag@gmail.com>
2023-02-09 16:23:42 +0100

    [py/jobs] Add earnings calc file

---
+++ python/jobs/rates_calc.py
@@ -0,0 +1,20 @@
+"""
+numbers based on https://www.wko.at/service/arbeitsrecht-sozialrecht/Neue_Selbststaendige_einfach.html
+"""
+
+# absolute
+MAX_BASE_EARNING = 6615
+
+# percentage
+HEALTH_INSURANCE = 6.80
+
+# percentage
+PENSION_INSURANCE = 18.5
+
+# percentage
+SELFEMPLOYED_PROVISION = 1.53
+
+# absolute
+ACCIDENT_INSURANCE = 10.64
+
+
