193502bc950ec06f6fb04c6c3e9674e6dc289d42
M. Sonntag <michael.p.sonntag@gmail.com>
2023-02-09 16:33:40 +0100

    [py/jobs] Add basic calculation

--- python/jobs/rates_calc.py
+++ python/jobs/rates_calc.py
@@ -1,5 +1,7 @@
 """
 numbers based on https://www.wko.at/service/arbeitsrecht-sozialrecht/Neue_Selbststaendige_einfach.html
+
+NOTE: base earning should be higher since there is no 13th salary...
 """
 
 # absolute
@@ -12,9 +14,18 @@ HEALTH_INSURANCE = 6.80
 PENSION_INSURANCE = 18.5
 
 # percentage
-SELFEMPLOYED_PROVISION = 1.53
+SELF_EMPLOYED_PROVISION = 1.53
 
 # absolute
 ACCIDENT_INSURANCE = 10.64
 
+# move this to a func
+earning = MAX_BASE_EARNING
 
+hin = (earning/100)*HEALTH_INSURANCE
+pin = (earning/100)*PENSION_INSURANCE
+sep = (earning/100)*SELF_EMPLOYED_PROVISION
+
+commission = hin+pin+sep+ACCIDENT_INSURANCE
+
+print(f"{commission:.2f}")
