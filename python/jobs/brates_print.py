8efaf045a7c1509ea8263d61f267609cc2e1fcf2
M. Sonntag <michael.p.sonntag@gmail.com>
2023-02-28 11:02:37 +0100

    [py/job/brates] Add base file

---
+++ python/jobs/brates_print.py
@@ -0,0 +1,15 @@
+def run():
+    years = 5
+    gogreen = 3.11
+    online = 2.22
+    month = years*12
+    for i in range(month):
+        currval = i+1
+        greenval = (currval-12)*gogreen
+        if greenval < 0:
+            greenval = 0
+        print(f"Month {currval}: online {currval*online} gogreen {greenval}")
+
+
+if __name__ == "__main__":
+    run()
