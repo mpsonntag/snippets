6102e062b024e9eece04bee200d6a8c44753b382
M. Sonntag <michael.p.sonntag@gmail.com>
2023-02-28 11:31:00 +0100

    [py/job/brates] Add final print

--- python/jobs/brates_print.py
+++ python/jobs/brates_print.py
@@ -1,14 +1,20 @@
 def run():
-    years = 5
+    years = 10
     gogreen = 3.11
     online = 2.22
     month = years*12
+    currval = 0
+    greenval = 0
     for i in range(month):
         currval = i+1
         greenval = (currval-12)*gogreen
         if greenval < 0:
             greenval = 0
         print(f"Month {currval}: online {currval*online:.2f} gogreen {greenval:.2f}")
+
+    print(f"\n-- Final rates after {years} years")
+    print(f"-- Month {currval}: online {currval*online:.2f} gogreen {greenval:.2f}")
+    print(f"-- gogreen final expenses: {greenval-currval*online:.2f}")
 
 
 if __name__ == "__main__":
