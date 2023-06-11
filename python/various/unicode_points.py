d4db9afcd96db37f3c655455d06dfcbd4bc70971
M. Sonntag <michael.p.sonntag@gmail.com>
2023-06-11 16:47:31 +0200

    [py/var/codepoints] Add hex range of interest

--- python/various/unicode_points.py
+++ python/various/unicode_points.py
@@ -4,7 +4,13 @@ test script to range through unicode code points
 
 
 def run():
-    print("Print ogham code points")
+    # unicode code points are provided as hexadecimal in the unicode consortium
+    # code charts (Unicode version 15.0; www.unicode.org)
+
+    # rough ogham hexadecimal code point range
+    hex_start = 0x1680
+    hex_end = 0x1700
+    print("Print ogham alphabet from unicode code hex points")
 
 
 if __name__ == "__main__":
