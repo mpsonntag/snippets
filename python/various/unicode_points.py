96ec3fcc0e31a3210c32ccb95465da2d12a96e87
M. Sonntag <michael.p.sonntag@gmail.com>
2023-06-11 16:50:46 +0200

    [py/var/codepoints] Range and print codepoints

--- python/various/unicode_points.py
+++ python/various/unicode_points.py
@@ -1,16 +1,20 @@
 """
 test script to range through unicode code points
 """
+import unicodedata as udat
 
 
 def run():
     # unicode code points are provided as hexadecimal in the unicode consortium
     # code charts (Unicode version 15.0; www.unicode.org)
 
-    # rough ogham hexadecimal code point range
+    # rough Ogham hexadecimal code point range
     hex_start = 0x1680
     hex_end = 0x1700
-    print("Print ogham alphabet from unicode code hex points")
+    print("Print Ogham alphabet from unicode code hex points")
+    # hexadecimal range and print of code points
+    for i in range(hex_start, hex_end):
+        print(f"{hex(i)}: {chr(i)} ({udat.category(chr(i))})")
 
 
 if __name__ == "__main__":
