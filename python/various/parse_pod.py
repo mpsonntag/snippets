e85f498bc612849e61e700bd2ff122f511a939a2
M. Sonntag <michael.p.sonntag@gmail.com>
2024-07-31 15:33:06 +0200

    [py/feedparser] Add argparse

--- python/various/parse_pod.py
+++ python/various/parse_pod.py
@@ -1,11 +1,27 @@
+import argparse
 import feedparser
 
-feed_url = ""
 
-curr_feed = feedparser.parse(feed_url)
+def handle_feed_url(feed_url):
+    curr_feed = feedparser.parse(feed_url)
 
-num_episodes = len(curr_feed.entries)
-curr_loop = len(curr_feed.entries)
-for en in curr_feed.entries:
-    print(f"#{curr_loop}/{num_episodes}: {en.title}: {en.link}")
-    curr_loop = curr_loop - 1
+    num_episodes = len(curr_feed.entries)
+    curr_loop = len(curr_feed.entries)
+    print("")
+    for en in curr_feed.entries:
+        print(f"#{curr_loop}/{num_episodes}: {en.title}: {en.link}")
+        curr_loop = curr_loop - 1
+    print("")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse RSS feed XML")
+    parser.add_argument("feed_url", help="RSS feed XML url")
+    args = parser.parse_args()
+
+    feed_url = args.feed_url
+    handle_feed_url(feed_url)
+
+
+if __name__ == "__main__":
+    main()
