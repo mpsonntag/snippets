712417b1dc52a5aecd7f8c8cba7f52ff09a8d21a
M. Sonntag <michael.p.sonntag@gmail.com>
2024-08-07 23:14:53 +0200

    [py/fetchfile] Basic cleanup

--- python/various/fetchfile.py
+++ python/various/fetchfile.py
@@ -4,17 +4,18 @@ import time
 from os.path import splitext
 
 offset = 0
-fn_base = "11_"
-fn = "linklist.md"
-with open(fn) as openfile:
+file_name_base = "11_"
+
+infile = "linklist.md"
+with open(infile, encoding="utf8") as openfile:
     while line := openfile.readline():
         line = line.strip()
-        offset+=1
+        offset += 1
         if not (offset % 10) % 5:
-            time.sleep(7)        
+            time.sleep(7)
         else:
             time.sleep(offset % 5)
-        _, currext = splitext(line)
-        currfn = f"{fn_base}{offset:04}{currext}"
-        print(f"Fetching {currfn}: {line}")
-        subprocess.run(["curl", line, "-o", currfn])
+        _, curr_ext = splitext(line)
+        curr_file_name = f"{file_name_base}{offset:04}{curr_ext}"
+        print(f"Fetching {curr_file_name}: {line}")
+        subprocess.run(["curl", line, "-o", curr_file_name], check=False)
