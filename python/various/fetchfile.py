67ab1876a03f14baf3a3cb341d0506b4427d58d0
M. Sonntag <michael.p.sonntag@gmail.com>
2024-08-12 11:06:58 +0200

    [py/fetchfile] Move file handling to own func

--- python/various/fetchfile.py
+++ python/various/fetchfile.py
@@ -8,26 +8,38 @@ import time
 from os.path import splitext
 
 
-def fetch_files(infile, offset=0, file_name_base=""):
+def read_file_content(infile):
     """
-    Open provided input file and try to fetch the content for each
-    entry via curl. The file extension is extracted from each URL.
+    Opens provided input file and returns the content as
+    a list of line entries with leading and trailing
+    whitespaces removed.
+    """
+    content = []
+    with open(infile, encoding="utf8") as openfile:
+        while line := openfile.readline():
+            content.append(line.strip())
+    return content
+
+
+def fetch_files(content, offset=0, file_name_base=""):
+    """
+    Try to fetch the content file for each entry from a provided list using curl.
+    The file extension is extracted from each URL.
     Files are serially numbered and the numbers are padded with zeroes to
     four digits.
     """
-    with open(infile, encoding="utf8") as openfile:
-        while line := openfile.readline():
-            line = line.strip()
-            offset += 1
-            if not (offset % 10) % 5:
-                time.sleep(7)
-            else:
-                time.sleep(offset % 5)
-            _, curr_ext = splitext(line)
-            curr_file_name = f"{file_name_base}{offset:04}{curr_ext}"
-            print(f"Fetching {curr_file_name} from {line}")
-            subprocess.run(["curl", line, "-o", curr_file_name], check=False,
-                           stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    for line in content:
+        line = line.strip()
+        offset += 1
+        if not (offset % 10) % 5:
+            time.sleep(7)
+        else:
+            time.sleep(offset % 5)
+        _, curr_ext = splitext(line)
+        curr_file_name = f"{file_name_base}{offset:04}{curr_ext}"
+        print(f"Fetching {curr_file_name} from {line}")
+        subprocess.run(["curl", line, "-o", curr_file_name], check=False,
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def main():
@@ -43,9 +55,14 @@ def main():
     args = parser.parse_args()
 
     infile = args.url_file
+    content = read_file_content(infile)
+    if not content:
+        print(f"Empty file {infile}")
+        return
+
     offset = args.offset if args.offset else 0
     file_name_base = args.name_base if args.name_base else ""
-    fetch_files(infile, offset, file_name_base)
+    fetch_files(content, offset, file_name_base)
 
 
 if __name__ == "__main__":
