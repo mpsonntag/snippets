34affb895f59e8eb8c299c586af86edde749a2a3
M. Sonntag <michael.p.sonntag@gmail.com>
2025-04-07 15:15:24 +0200

    [py/weather] Fetch content from URL

--- python/various/weather.py
+++ python/various/weather.py
@@ -3,6 +3,23 @@ Fetch and process weather information.
 """
 
 import argparse
+import urllib.request as ureq
+
+
+def fetch_url_content(url_str):
+    """
+    Fetch and return content from a URL as string
+    """
+    content = ""
+    response = ureq.urlopen(url_str)
+    code = response.getcode()
+    if code == 200:
+        print(f"Response status: {code}")
+        content = response.read()
+    else:
+        print(f"Response status: {code}")
+
+    return content
 
 
 def main():
@@ -14,7 +31,8 @@ def main():
 
     args = parser.parse_args()
     url_str = args.url_str
-    print(url_str)
+
+    content = fetch_url_content(url_str)
 
 
 if __name__ == "__main__":
